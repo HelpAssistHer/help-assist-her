254bb8b8fbffc027310e7fa66dd53d83e7878fe5
Karen Rose <karen.rose807@gmail.com>
2016-12-21 22:02:51 -0800

    Testing port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const express = require('express')
 
 const server = express()
 
-const port = config.server.port
+// const port = config.server.port
+const port = process.env.PORT
 
 server.get('/', function (req, res) {
 	res.send('Hello World!')
