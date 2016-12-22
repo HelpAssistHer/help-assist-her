544ec4f26a7b11d8c274579ed68edbead2e7648f
Karen Rose <karen.rose807@gmail.com>
2016-12-21 22:01:08 -0800

    Added prod config

---
+++ server/config/production.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+	server: {
+		hostname: 'hah-dev.herokuapp.com',
+		port: process.env.PORT,
+		protocol: 'https',
+		url: 'https://hah-dev.herokuapp.com',
+	},
+}
