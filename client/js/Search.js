3a93c2220f1825a0e78fe8e45d8c139b40203bb0
Karen Rose <karen.rose807@gmail.com>
2017-02-01 18:16:56 -0800

    More react files

---
+++ client/js/Search.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+
+const Search = React.createClass({
+	render () {
+		return (
+			<div className="search">
+				<h1>Search Page</h1>
+			</div>
+		)
+	}
+})
+
+export default Search
