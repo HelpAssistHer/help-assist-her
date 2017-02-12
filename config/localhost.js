25c66545be9526517235f2c622339b16af3a1797
Karen Rose <karen.rose807@gmail.com>
2017-02-12 13:16:07 -0800

    Add mapbox access token

--- config/localhost.js
+++ config/localhost.js
@@ -4,6 +4,9 @@ module.exports = {
 	database: {
 		name: 'hah-dev'
 	},
+	mapbox: {
+		accessToken: 'pk.eyJ1Ijoia2FyZW5tcm9zZSIsImEiOiJjaXd6bWoyeXowMGg2MnRvNnlncWE0azZsIn0.R61g_76oVqY3Jg7ob4kSsA',
+	},
 	server: {
 		hostname: 'localhost',
 		port: 4000,
