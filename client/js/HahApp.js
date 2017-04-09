7738faffc30f74d39fca082843a62bfd5d2b63a1
Karen Rose <karen.rose807@gmail.com>
2017-04-08 22:26:17 -0700

    Added header

--- client/js/HahApp.js
+++ client/js/HahApp.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
 
+import Header from './header'
 import Landing from './Landing'
 import Search from './Search'
 import VerificationPortal from './verification-portal-index'
@@ -11,13 +12,16 @@ import VerificationPortal from './verification-portal-index'
 const App = React.createClass({
 	render() {
 		return (
-			<BrowserRouter>
-				<div className='app'>
-					<Match exactly pattern='/' component={Landing} />
-					<Match pattern='/search' component={Search} />
-					<Match pattern='/verification' component={VerificationPortal} />
-				</div>
-			</BrowserRouter>
+			<div>
+				<Header/>
+				<BrowserRouter>
+					<div className='app'>
+						<Match exactly pattern='/' component={Landing} />
+						<Match pattern='/search' component={Search} />
+						<Match pattern='/verification' component={VerificationPortal} />
+					</div>
+				</BrowserRouter>
+			</div>
 		)
 	}
 })
