eb43dd9bafcfe268497188e3bc31dbff1c3f94f3
Karen Rose <karen.rose807@gmail.com>
2017-04-21 08:32:05 -0700

    Remove placeholders

--- client/js/verification-portal-view.js
+++ client/js/verification-portal-view.js
@@ -18,42 +18,34 @@ const VerificationPortalView = ({ classes, info, getResourceClick, submitResourc
 		<div className={classes.fields}>
 			<Input label='Name'
 				   value={ info.name }
-				   placeholder='Enter Name'
 				   id='name'
 			/>
 			<Input label='Address 1'
 				   value={ info.address1 }
-				   placeholder='Enter Address 1'
 				   id='address1'
 			/>
 			<Input label='Address 2'
 				   value={ info.address2 }
-				   placeholder='Enter Address 2'
 				   id='address2'
 			/>
 			<Input label='City'
 				   value={ info.city }
-				   placeholder='Enter City'
 				   id='city'
 			/>
 			<Input label='State'
 				   value={ info.state }
-				   placeholder='Enter State'
 				   id='state'
 			/>
 			<Input label='Zip Code'
 				   value={ info.zipCode }
-				   placeholder='Enter Zip Code'
 				   id='zipCode'
 			/>
 			<Input label='Phone Number'
 				   value={ info.phone }
-				   placeholder='Enter Phone Number'
 				   id='phone'
 			/>
 			<Input label='Website'
 				   value={ info.website }
-				   placeholder='Enter Website'
 				   id='website'
 			/>
 		</div>
@@ -62,22 +54,18 @@ const VerificationPortalView = ({ classes, info, getResourceClick, submitResourc
 		<h3>Primary Contact</h3>
 			<Input label='First Name'
 				   value={ info.primaryContactFirstName }
-				   placeholder='First Name'
 				   id='primary_contact_first_name'
 			/>
 			<Input label='Last Name'
 				   value={ info.primaryContactLastName }
-				   placeholder='Enter Last Name'
 				   id='primary_contact_last_name'
 			/>
 			<Input label='Email'
 				   value={ info.primaryContactEmail }
-				   placeholder='Enter Email'
 				   id='primary_contact_email'
 			/>
 			<Input label='Phone'
 				   value={ info.primaryContactPhone }
-				   placeholder='Enter Phone'
 				   id='primary_contact_phone'
 			/>
 		</div>
