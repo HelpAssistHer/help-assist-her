4c47ad49e8e585ba121daaea90ae41e67f4305ae
Katelyn Sills <katelynsills@gmail.com>
2017-05-21 14:18:02 -0700

    removed the try/catch block around truncating the PregnancyCenterModel so that we don't continue if it fails. Also reorganized the imports to match the alphabetical format of the server.js file.

--- server/data-import/data.js
+++ server/data-import/data.js
@@ -1,15 +1,17 @@
 'use strict'
 
 const config = require('config')
+const EJSON = require('mongodb-extended-json')
+const fs = require('fs')
+const Joi = require('joi')
 const Log = require('log')
 const mongoose = require('mongoose')
 const P = require('bluebird')
-mongoose.Promise = require('bluebird')
-const fs = require('fs')
+
 const PregnancyCenterModel = require('../pregnancy-centers/schema/mongoose-schema')
-const EJSON = require('mongodb-extended-json')
-const Joi = require('joi')
 const pregnancyCenterSchemaJoi = require('../pregnancy-centers/schema/joi-schema')
+
+mongoose.Promise = require('bluebird')
 const log = new Log('info')
 
 const loadData = P.coroutine(function *startDatabase() {
@@ -23,20 +25,16 @@ loadData()
 // note that exports are 'mongoexport --db hah-dev --collection pregnancycenters --jsonArray --out cessilye_nypc_geocoded.json'
 
 async function reimport() {
-
 	let docs
 
-	try {
-		await PregnancyCenterModel.collection.drop()
-	} catch (err) {
-		log.error(err)
-	}
+	await PregnancyCenterModel.collection.drop()
 
 	try {
 		const dataToImport = fs.readFileSync('server/data-import/test/fixtures/cessilye_nypc.json', 'utf8')
 		docs = EJSON.parse(dataToImport)
 	} catch (err) {
 		log.error(err)
+		process.exit()
 	}
 
 	for (const doc of docs) {
