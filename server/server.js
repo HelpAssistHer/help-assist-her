3a4a91d34a4eb9e2228e8bf263bf2375a195350c
Katelyn Sills <katelynsills@gmail.com>
2017-05-21 18:45:37 -0700

    Travis CI correctly fails on linter failing, now trying to test things that will pass the linter but fail on running the server.

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ server.use(session({
 }))
 server.use(passport.initialize())
 server.use(passport.session())
-(
+
 passport.use(
 	new facebookStrategy({
 		clientID: config.facebook.appId,
@@ -369,3 +369,5 @@ function createUpdateHistory(req, pregnancyCenterRawObj) {
 }
 
 module.exports = server
+
+throw new Error()
