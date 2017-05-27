a087ffa70c0f338b12c382f4a1346275d6a747dd
Karen Rose <karen.rose807@gmail.com>
2017-05-27 16:20:48 -0700

    Add build script

---
+++ webpack.prod.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+	devtool: 'source-map',
+
+	entry: [
+		'./client/js/hah-app/index.js'
+	],
+
+	externals: {
+		'clientConfig': JSON.stringify(process.env.ENV === 'dev' ? {
+			serverUrl: 'https://hah-dev.herokuapp.com'
+		} : {
+			serverUrl: 'http://localhost:4000'
+		})
+	},
+
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js',
+		publicPath: '/public/'
+	},
+
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			minimize: true,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	],
+
+	module: {
+		loaders: [
+			{ test: /\.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/ },
+			{ test: /\.scss?$/,
+				loader: 'style!css!sass',
+				include: path.join(__dirname, 'src', 'styles') },
+			{ test: /\.png$/,
+				loader: 'file' },
+			{ test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				loader: 'file'}
+		]
+	}
+}
