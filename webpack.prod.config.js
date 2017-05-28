5fb588ed8bfa3df39cf163878a57af25a3dede60
Karen Rose <karen.rose807@gmail.com>
2017-05-27 18:41:43 -0700

    Webpack changes

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -30,17 +30,21 @@ module.exports = {
 				warnings: false
 			}
 		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('dev')
-			}
-		})
+		// new webpack.DefinePlugin({
+		// 	'process.env': {
+		// 		'NODE_ENV': JSON.stringify('dev')
+		// 	}
+		// })
 	],
 
+	resolve: {
+		extensions: ['.js', '.json']
+	},
+
 	module: {
-		loaders: [
+		rules: [
 			{
-				test: /\.js?$/,
+				test: /\.js$/,
 				loader: 'babel-loader',
 				include: path.resolve(__dirname, 'client/js'),
 				exclude: /node_modules/
@@ -49,6 +53,10 @@ module.exports = {
 				test: /\.png$/,
 				loader: 'file'
 			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader'
+			}
 		]
 	}
 }
