07c026f4b0474bb178ef4ad0ba0052e108cbacb0
Karen Rose <karen.rose807@gmail.com>
2017-06-03 16:03:25 -0700

    Remove extra stuff from landing

--- client/js/hah-app/landing.js
+++ client/js/hah-app/landing.js
@@ -1,24 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const resources = require('./list-of-resources.json')
-
 class Landing extends React.Component {
 	render() {
 		return (
 			<div className='landing'>
-				<h1>Welcome to HAH</h1>
 				<Link to='/verification'>Verification Portal</Link>
-				<input type='text' placeholder='Search' />
-
-				{ resources.map((resource => {
-					return (
-						<Link to='/search' key={resource.id}>{resource.name}</Link>
-					)
-				}))}
-
-				<Link to='/search'>Just in case I forget</Link>
-				<pre><code>{ 'This is a good way to debug' }</code></pre>
 			</div>
 		)
 	}
