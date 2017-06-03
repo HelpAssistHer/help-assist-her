9833e866e5c1bd41d1b04a73968e54228b657542
Karen Rose <karen.rose807@gmail.com>
2017-06-03 15:02:17 -0700

    Testing

--- client/js/authentication/action-creators.js
+++ client/js/authentication/action-creators.js
@@ -1,7 +1,7 @@
 import { serverUrl } from 'clientConfig'
 
 export async function authenticateUser(accessToken) {
-	const response = await fetch(`${serverUrl}/auth/facebook/token?access_token=${accessToken}`, {
+	const response = await fetch(`/auth/facebook/token?access_token=${accessToken}`, {
 		method: 'GET',
 		credentials: 'include',
 		headers: {
