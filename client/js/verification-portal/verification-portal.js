93d4d7ec15a8eff81869a780fdfa61486163535b
Karen Rose <karen.rose807@gmail.com>
2017-08-02 11:25:24 -0700

    Add styles directly to verification portal component

--- client/js/verification-portal/verification-portal.js
+++ client/js/verification-portal/verification-portal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 
-import ContainerFluid from '../components/container-fluid'
 import GetResourceToVerifyButton from './get-resource-to-verify-button'
 import Spacer from '../components/spacer'
 import VerificationPortalForm from './form'
@@ -13,33 +12,35 @@ class VerificationPortal extends React.Component {
 	}
 	render() {
 		const { classes, changeFieldValue } = this.props
-		return (
-			<ContainerFluid content={
-				<div className={classes.verificationPortal}>
-					<h1>VERIFICATION PORTAL</h1>
-					<GetResourceToVerifyButton
-						changeFieldValue={changeFieldValue}
-					/>
 
-					<VerificationPortalForm
-						onSubmit={this.submit}
-					/>
-					<Spacer height='100px'/>
-				</div>
-			} />
-			
+		return (
+			<div className={classes.verificationPortal}>
+				<h1>VERIFICATION PORTAL</h1>
+				<GetResourceToVerifyButton
+					changeFieldValue={changeFieldValue}
+				/>
+
+				<VerificationPortalForm
+					onSubmit={this.submit}
+				/>
+				<Spacer height='100px'/>
+			</div>
 		)
 	}
 }
 
-
 const styles = {
 	verificationPortal: {
 		'text-align': 'center',
-		
-	}
+		'font-family': 'sans-serif',
+		color: '#4A4A4A',
+		'max-width': '903px',
+		'background-color': '#ffffff',
+		'margin-right': 'auto',
+		'margin-left': 'auto',
+		'padding-left': '15px',
+		'padding-right': '15px',
+	},
 }
 
-VerificationPortal = injectSheet(styles)(VerificationPortal)
-
-export default VerificationPortal
+export default injectSheet(styles)(VerificationPortal)
