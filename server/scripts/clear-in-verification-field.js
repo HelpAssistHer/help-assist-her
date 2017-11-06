b87f019f98f11a9e5a4a8484516edb222b7daf43
Katelyn Sills <katelynsills@gmail.com>
2017-11-06 12:35:12 -0800

    ensure that the script to clear the `inVerification` field works

--- server/scripts/clear-in-verification-field.js
+++ server/scripts/clear-in-verification-field.js
@@ -1,10 +1,13 @@
 'use strict'
 
 const config = require('config')
+const Log = require('log')
 const mongoose = require('mongoose')
 const P = require('bluebird')
 const PregnancyCenterModel = require('../pregnancy-centers/schema/mongoose-schema')
 mongoose.Promise = require('bluebird')
+
+const log = new Log('info')
 
 // TODO: Error handling
 const startDatabase = P.coroutine(function *startDatabase() {
@@ -15,9 +18,13 @@ const startDatabase = P.coroutine(function *startDatabase() {
 startDatabase()
 
 async function clearInVerificationField() {
-	const query = {}
+	const query = { inVerification: {$exists: true}}
 	const update = {inVerification: null}
-	await PregnancyCenterModel.update(query, update)
+	try {
+		await PregnancyCenterModel.update(query, update, {multi: true})
+	} catch (err) {
+		log.error(err)
+	}
 	process.exit()
 }
 
