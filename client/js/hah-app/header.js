049dfa7e1a5ca86da03b23c4e0c93fb00f775a76
bhavyaab <bhavyab@outlook.com>
2018-05-15 20:22:46 -0700

    cleanup

--- client/js/hah-app/header.js
+++ client/js/hah-app/header.js
@@ -4,29 +4,10 @@ import LoginButton from '../authentication/facebook-login-button'
 
 const Header = ({ classes }) => (
 	<div className={classes.header}>
-		<div className={classes.authenticationButtonDiv}>
 			<LoginButton />
-		</div>
 	</div>
 )
 
-const styles = {
-	header: {
-		position: 'relative',
-	},
-	logo: {
-		'max-height': '100%',
-		'max-width': '100%',
-		width: 'auto',
-		height: 'auto',
-		position: 'absolute',
-		top: '0',
-		bottom: '0',
-		left: '0',
-		right: '0',
-		margin: 'auto',
-	},
-	authenticationButtonDiv: {},
-}
+const styles = {}
 
 export default injectSheet(styles)(Header)
