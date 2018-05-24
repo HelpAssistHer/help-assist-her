220ae5f3640c03862c883a6d057b0c6ccb6c67b4
Karen Rose <karen.rose807@gmail.com>
2018-05-23 19:38:15 -0700

    Format file with prettier

--- client/js/verification-portal/verification-portal.js
+++ client/js/verification-portal/verification-portal.js
@@ -9,50 +9,44 @@ import MediumButton from '../components/medium-button' // importing bewBUtton Co
 import { shouldShowFeature } from '../hah-app/helpers'
 
 class VerificationPortal extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props)
 		this.state = {
-			outOfBiz: false // addition of variable to keep track of out of business status.
+			outOfBiz: false, // addition of variable to keep track of out of business status.
 		}
 		this.handleClick = this.handleClick.bind(this)
 	}
-	handleClick = () => { // toggling out of business status
-		let {outOfBiz} = this.state
+	handleClick = () => {
+		// toggling out of business status
+		let { outOfBiz } = this.state
 		this.setState({ outOfBiz: !outOfBiz })
 	}
-	submit = (values) => {
+	submit = values => {
 		updateResource(values)
 	}
 	render() {
 		const userDisplayName = _.get(this.props, 'initialData.userDisplayName')
 		const { classes, changeFieldValue } = this.props
 		return (
-		<div>
-			<div className={classes.leftPositionButton}>
-				<GetResourceToVerifyButton
-					changeFieldValue={changeFieldValue}
-					/>
-			</div>
-			<div className={classes.verificationPortal}>
-				{
-					shouldShowFeature(userDisplayName) && (
+			<div>
+				<div className={classes.leftPositionButton}>
+					<GetResourceToVerifyButton changeFieldValue={changeFieldValue} />
+				</div>
+				<div className={classes.verificationPortal}>
+					{shouldShowFeature(userDisplayName) && (
 						<div className={classes.leftPositionButton}>
-							<MediumButton btnType={this.state.outOfBiz? 'orange':'blackAndWhite'}
-										  buttonText='Out of Business'
-										  onClick={this.handleClick}>
-							</MediumButton>
+							<MediumButton
+								btnType={this.state.outOfBiz ? 'orange' : 'blackAndWhite'}
+								buttonText="Out of Business"
+								onClick={this.handleClick}
+							/>
 						</div>
-					)
-				}
+					)}
 
-
-       <VerificationPortalForm
-			    outOfBiz={this.state.outOfBiz}
-          onSubmit={this.submit}
-        />
-				<Spacer height='100px'/>
+					<VerificationPortalForm outOfBiz={this.state.outOfBiz} onSubmit={this.submit} />
+					<Spacer height="100px" />
+				</div>
 			</div>
-		</div>
 		)
 	}
 }
@@ -61,20 +55,20 @@ const styles = {
 	verificationPortal: {
 		'text-align': 'center',
 		'font-family': 'sans-serif',
-		'color': '#4A4A4A',
+		color: '#4A4A4A',
 		'max-width': '903px',
 		'margin-left': 'calc(50% - 451.5px)',
 		'background-color': '#ffffff',
 		'padding-left': '15px',
 		'padding-right': '15px',
-		'position': 'relative',
+		position: 'relative',
 	},
-	leftPositionButton:{
-		'width': '100%',
-		'position': 'absolute',
-		'left': '1%',
-		'z-index':'100',
-	}
+	leftPositionButton: {
+		width: '100%',
+		position: 'absolute',
+		left: '1%',
+		'z-index': '100',
+	},
 }
 
 export default injectSheet(styles)(VerificationPortal)
