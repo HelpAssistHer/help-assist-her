94462ed61e538152a7aca5d764c6a4a93835050d
Karen Rose <karen.rose807@gmail.com>
2018-05-26 10:32:10 -0700

    Add Valery to dev list

--- client/js/hah-app/helpers.js
+++ client/js/hah-app/helpers.js
@@ -1,6 +1,6 @@
 const R = require('ramda')
 
-const hahSoftwareEngineers = ['Karen Rose', 'Kate Sills', 'Bhavya Bharti']
+const hahSoftwareEngineers = ['Karen Rose', 'Kate Sills', 'Bhavya Bharti', 'Valery Zajkov']
 
 export const shouldShowFeature = userDisplayName => {
 	return R.contains(userDisplayName, hahSoftwareEngineers)
