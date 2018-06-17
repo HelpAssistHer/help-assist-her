3354bf3ea4e345d8f8951fdc353a9b3ddf699ee2
Karen Rose <karen.rose807@gmail.com>
2018-06-17 14:46:55 -0700

    Remove unused code

--- client/js/verification-portal/index.js
+++ client/js/verification-portal/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import injectSheet from 'react-jss'
 
 import HeaderSuccess from './components/header-success'
-import Spacer from '../components/spacer'
 import LoginButton from '../authentication/facebook-login-button'
 import { formTypes } from './constants'
 import CommunityHealthCenterForm from './community-health-center'
@@ -23,14 +21,10 @@ const VerificationPortal = ({ classes, formType }) => {
 	return (
 		<div>
 			<HeaderSuccess />
-			<Spacer height="64px" />
-			<Spacer height="25px" />
 			<LoginButton />
 			{getFormComponent({ formType })}
 		</div>
 	)
 }
 
-const styles = {}
-
-export default injectSheet(styles)(VerificationPortal)
+export default VerificationPortal
