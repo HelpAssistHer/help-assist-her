dfcb8a515cbc21fd1ab6f17c067070a0ca3887d2
Karen Rose <karen.rose807@gmail.com>
2018-06-17 08:13:40 -0700

    Move verification portal form code to new file

---
+++ client/js/verification-portal/verification-portal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import injectSheet from 'react-jss'
+
+import HeaderSuccess from './components/header-success'
+import Tabs from './components/tabs'
+import LoginButton from '../authentication/facebook-login-button'
+
+const VerificationPortal = ({ classes }) => (
+	<div>
+		<HeaderSuccess />
+		<Tabs tabNames={['community health center', 'pregnancy resource center']} />
+		<Spacer height="64px" />
+		<Spacer height="25px" />
+		<LoginButton />
+	</div>
+)
+
+const styles = {}
+
+export default injectSheet(styles)(VerificationPortal)
--- client/js/verification-portal/view.js
+++ client/js/verification-portal/view.js
@@ -2,9 +2,6 @@ import React from 'react'
 import injectSheet from 'react-jss'
 
 import GetResourceToVerifyButton from './get-resource-to-verify-button'
-import HeaderSuccess from './components/header-success'
-import Tabs from './components/tabs'
-import LoginButton from '../authentication/facebook-login-button'
 import Spacer from '../components/spacer'
 import VerificationPortalForm from './form'
 import { updateResource } from './action-creators'
@@ -33,15 +30,8 @@ class VerificationPortal extends React.Component {
 
 		return (
 			<div>
-				<HeaderSuccess />
-				<Tabs
-					tabNames={['community health center', 'pregnancy resource center']}
-				/>
-				<Spacer height="64px" />
 				<div className={classes.leftPositionButton}>
 					<GetResourceToVerifyButton changeFieldValue={changeFieldValue} />
-					<Spacer height="25px" />
-					<LoginButton />
 				</div>
 				<div className={classes.rightPositionButton}>
 					<Button
