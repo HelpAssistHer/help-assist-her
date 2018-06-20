d308847bf308291508c87d070714db6abfaab69c
Katelyn Sills <katelynsills@gmail.com>
2018-06-19 20:50:38 -0700

    remove swagger

--- server/documentation/controllers/Default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Default = require('./DefaultService');
-
-module.exports.rootGET = function rootGET (req, res, next) {
-  Default.rootGET(req.swagger.params, res, next);
-};
