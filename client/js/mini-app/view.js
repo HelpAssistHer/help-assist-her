1ae1513c9de4aefc2e8c2f07ff4b71cb3b839055
Karen Rose <karen.rose807@gmail.com>
2018-07-31 23:05:30 -0700

    Cleanup and get fully working

--- client/js/mini-app/view.js
+++ client/js/mini-app/view.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import injectSheet from 'react-jss'
+import { withRouter } from 'react-router-dom'
 
 import MiniAppForm from './form'
 import { getPregnancyResourceCenters } from './data/action-creators'
 
 class MiniApp extends Component {
 	submit = ({ locationInput }) => {
-		const { dispatch } = this.props
+		const { dispatch, history } = this.props
 		dispatch(getPregnancyResourceCenters(locationInput))
+		history.push('/mini-app/pregnancy-resource-centers')
 	}
 
 	render() {
@@ -26,4 +29,6 @@ const styles = {
 	},
 }
 
-export default injectSheet(styles)(MiniApp)
+const MiniAppStyle = injectSheet(styles)(MiniApp)
+
+export default connect()(withRouter(MiniAppStyle))
