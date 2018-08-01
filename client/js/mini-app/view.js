4405aad70e97c5c1ca071bc6ade07cf80783601a
Karen Rose <karen.rose807@gmail.com>
2018-07-31 22:15:05 -0700

    Add submit

--- client/js/mini-app/view.js
+++ client/js/mini-app/view.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import injectSheet from 'react-jss'
 
 import Input from '../components/input'
 import ResourceTypeButton from './components/resource-type-button'
 
-const MiniApp = ({ classes }) => {
-	return (
-		<div className={classes.root}>
-			The Mini App
-			<Field
-				placeholder="Address, city/state, zip code"
-				name="locationInput"
-				component={Input}
-				type="text"
-			/>
-			<ResourceTypeButton />
-		</div>
-	)
+class MiniApp extends Component {
+	submit = values => {
+		console.log('VALUES', values)
+	}
+
+	render() {
+		const { classes, handleSubmit } = this.props
+
+		return (
+			<form onSubmit={handleSubmit}>
+				<div className={classes.root}>
+					The Mini App
+					<Field
+						placeholder="Address, city/state, zip code"
+						name="locationInput"
+						component={Input}
+						type="text"
+					/>
+					<ResourceTypeButton />
+				</div>
+			</form>
+		)
+	}
 }
 
 const styles = {
