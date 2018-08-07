b7a86aa5522f3a707fc4d46c2ee54c242577ea7e
Karen Rose <karenmrose@users.noreply.github.com>
2018-08-07 14:14:44 -0700

    Geocode address (#100)
    This PR actually geocodes the address that's typed into the address input and displays results within 50 miles of that address!

--- client/js/mini-app/components/resource-type-button.js
+++ client/js/mini-app/components/resource-type-button.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import injectSheet from 'react-jss'
-import { withRouter } from 'react-router-dom'
 
-import { getPregnancyResourceCenters } from '../data/action-creators'
-
-const ResourceTypeButtonStyle = ({ classes, dispatch, history }) => {
+const ResourceTypeButton = ({ classes, onClick }) => {
 	return (
 		<div className={classes.root}>
-			<button
-				type="submit"
-				onClick={() => {
-					dispatch(getPregnancyResourceCenters())
-					history.push('/mini-app/pregnancy-resource-centers')
-				}}
-			>
+			<button type="submit" onClick={onClick}>
 				Pregnancy Centers
 			</button>
 		</div>
@@ -27,6 +17,4 @@ const styles = {
 	},
 }
 
-const ResourceTypeButton = injectSheet(styles)(ResourceTypeButtonStyle)
-
-export default connect()(withRouter(ResourceTypeButton))
+export default injectSheet(styles)(ResourceTypeButton)
