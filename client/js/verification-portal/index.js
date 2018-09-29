894999f57b1437b17e8f8d3c16d3643a149c9d7a
Valery Zajkov <wuvalzaj@gmail.com>
2018-09-29 17:49:37 -0400

    Import getFormComponent function

--- client/js/verification-portal/index.js
+++ client/js/verification-portal/index.js
@@ -1,30 +1,19 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 
-import HeaderModal from './components/header-modal'
-import Spacer from '../components/spacer'
-import LoginButton from '../authentication/facebook-login-button'
-import Tabs from './components/tabs'
 import CommunityHealthCenterForm from './community-health-center'
 import PregnancyResourceCenterForm from './pregnancy-resource-center'
 import { formTypes } from './constants'
 import LeftSideNavContainer from './left-side-nav-container'
-import ResourceView from './resource-view'
+import { ResourceView, getFormComponent } from './resource-view'
 
 const VerificationPortal = ({ formType, classes }) => {
 	return (
-		<div>
-			<HeaderModal />
-			<Tabs />
-			<Spacer height="63px" />
-			<LoginButton />
-			{getFormComponent({ formType })}
-			<div className={classes.verificationPortal}>
-				<div className={classes.leftSideNav}>
-					<LeftSideNavContainer />
-				</div>
-				<ResourceView formType={formType} />
+		<div className={classes.verificationPortal}>
+			<div className={classes.leftSideNav}>
+				<LeftSideNavContainer />
 			</div>
+			<ResourceView formType={formType} />
 		</div>
 	)
 }
