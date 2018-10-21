3d5c7a82044c37e6b63d5c3ff77530bb64b5fe28
Karen Rose <karen.rose807@gmail.com>
2018-10-21 15:48:42 -0700

    Style links in footer

--- client/js/mini-app/components/footer.js
+++ client/js/mini-app/components/footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import injectSheet from 'react-jss'
 
 const MiniAppFooter = ({ classes }) => {
-	return <div className={classes.footerRoot} />
+	return (
+		<div className={classes.footerRoot}>
+			<Link to="/about" className={classes.navigationLink}>
+				About Us
+			</Link>
+		</div>
+	)
 }
 
 const styles = {
@@ -10,6 +17,12 @@ const styles = {
 		height: '188px',
 		'background-color': '#3d65f9',
 		'margin-top': '100px',
+	},
+	navigationLink: {
+		'font-family': 'sans-serif',
+		'font-size': '18px',
+		color: '#ffffff',
+		'text-decoration': 'none',
 	},
 }
 
