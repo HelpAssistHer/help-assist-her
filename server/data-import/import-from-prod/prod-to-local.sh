b3af7f7af9938a29c16424ca0272ac6ed45ed429
Kate Sills <katelynsills@gmail.com>
2018-10-20 19:06:13 -0700

    add prod-to-local script, clean up

---
+++ server/data-import/import-from-prod/prod-to-local.sh
@@ -0,0 +1,3 @@
+mongodump -h ds119873-a0.mlab.com:19873 -d hah-prod -u hah_prod_backup -p $HAH_PROD_BACKUP -o tmp
+mongorestore -h localhost -d hah-local  --drop tmp/hah-prod
+rm -rf tmp/hah-prod
--- server/data-import/import-from-prod/prod-to-stage-and-dev.sh
+++ server/data-import/import-from-prod/prod-to-stage-and-dev.sh
@@ -1,4 +1,4 @@
-echo "$HAH_PROD_BACKUP" | mongodump -h ds119873-a0.mlab.com:19873 -d hah-prod -u hah_prod_backup -o tmp
+mongodump -h ds119873-a0.mlab.com:19873 -d hah-prod -u hah_prod_backup -p $HAH_PROD_BACKUP -o tmp
 mongorestore -h ds143539.mlab.com:43539 -d hah-stage -u hah_stage_restore -p $HAH_STAGE_RESTORE --drop tmp/hah-prod
 mongorestore -h ds143559.mlab.com:43559 -d hah-dev -u hah_dev_restore -p $HAH_DEV_RESTORE --drop tmp/hah-prod
 rm -rf tmp/hah-prod
