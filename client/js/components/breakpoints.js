1b3bfe83f335173652255d471a1197549f864f83
Karen Rose <karen.rose807@gmail.com>
2018-10-20 20:38:26 -0700

    Resolve error

--- client/js/components/breakpoints.js
+++ client/js/components/breakpoints.js
@@ -8,35 +8,37 @@ const breakpoints = {
 }
 
 const Breakpoint = props => {
-	const breakpoint = breakpoints[props.name] || breakpoints.phone
-	console.log('BREAKPOINT PROP', breakpoint)
-
 	return (
-		<MediaQuery {...props} minWidth={700}>
-			{props.children}
-		</MediaQuery>
+		<div>
+			<div>Device Test!</div>
+			<MediaQuery minDeviceWidth={1224}>
+				<div>You are a desktop or laptop</div>
+			</MediaQuery>
+			<MediaQuery maxWidth={1224}>
+				<div>You are sized like a tablet or mobile phone though</div>
+			</MediaQuery>
+			<MediaQuery minDeviceWidth={1824}>
+				<div>You also have a huge screen</div>
+			</MediaQuery>
+			<MediaQuery maxDeviceWidth={1224}>
+				<div>You are a tablet or mobile phone</div>
+			</MediaQuery>
+			<MediaQuery orientation="portrait">
+				<div>You are portrait</div>
+			</MediaQuery>
+			<MediaQuery orientation="landscape">
+				<div>You are landscape</div>
+			</MediaQuery>
+			<MediaQuery minResolution="2dppx">
+				<div>You are retina</div>
+			</MediaQuery>
+		</div>
 	)
 }
 
-export function DesktopBreakpoint(props) {
-	console.log('DESKTOP BREAKPOINT')
-	return <Breakpoint name="desktop">{props.children}</Breakpoint>
-}
-//
-// import Responsive from 'react-responsive';
-//
-// const Desktop = props => <Responsive {...props} minWidth={992} />;
-// const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
-// const Mobile = props => <Responsive {...props} maxWidth={767} />;
-// const Default = props => <Responsive {...props} minWidth={768} />;
-//
-// const Example = () => (
-// 	<div>
-// 		<Desktop>Desktop or laptop</Desktop>
-// 		<Tablet>Tablet</Tablet>
-// 		<Mobile>Mobile</Mobile>
-// 		<Default>Not mobile (desktop or laptop or tablet)</Default>
-// 	</div>
-// );
-//
-// export default Example;
+export default Breakpoint
+
+// export function DesktopBreakpoint(props) {
+// 	console.log('DESKTOP BREAKPOINT')
+// 	return <Breakpoint name="desktop">{props.children}</Breakpoint>
+// }
