5f38c26ed59e1ccc243b8ab0bb451498580d5520
Karen Rose <karen.rose807@gmail.com>
2018-10-20 21:20:34 -0700

    Get breakpoints to work

--- client/js/components/breakpoints.js
+++ client/js/components/breakpoints.js
@@ -1,44 +1,15 @@
 import React from 'react'
-import MediaQuery from 'react-responsive'
+import Responsive from 'react-responsive'
 
-const breakpoints = {
-	desktop: '(min-width: 1025px)',
-	tablet: '(min-width: 768px) and (max-width: 1024px)',
-	phone: '(max-width: 767px)',
-}
+const DESKTOP_MIN = 992
+const TABLET_MIN = 768
+const TABLET_MAX = 991
+const PHONE_MAX = 767
 
-const Breakpoint = props => {
-	return (
-		<div>
-			<div>Device Test!</div>
-			<MediaQuery minDeviceWidth={1224}>
-				<div>You are a desktop or laptop</div>
-			</MediaQuery>
-			<MediaQuery maxWidth={1224}>
-				<div>You are sized like a tablet or mobile phone though</div>
-			</MediaQuery>
-			<MediaQuery minDeviceWidth={1824}>
-				<div>You also have a huge screen</div>
-			</MediaQuery>
-			<MediaQuery maxDeviceWidth={1224}>
-				<div>You are a tablet or mobile phone</div>
-			</MediaQuery>
-			<MediaQuery orientation="portrait">
-				<div>You are portrait</div>
-			</MediaQuery>
-			<MediaQuery orientation="landscape">
-				<div>You are landscape</div>
-			</MediaQuery>
-			<MediaQuery minResolution="2dppx">
-				<div>You are retina</div>
-			</MediaQuery>
-		</div>
-	)
-}
+const Desktop = props => <Responsive {...props} minWidth={DESKTOP_MIN} />
+const Tablet = props => (
+	<Responsive {...props} minWidth={TABLET_MIN} maxWidth={TABLET_MAX} />
+)
+const Phone = props => <Responsive {...props} maxWidth={PHONE_MAX} />
 
-export default Breakpoint
-
-// export function DesktopBreakpoint(props) {
-// 	console.log('DESKTOP BREAKPOINT')
-// 	return <Breakpoint name="desktop">{props.children}</Breakpoint>
-// }
+export { Desktop, Tablet, Phone }
