5bbad36e0086e9ad5ff91b7a47d214c952c72264
Karen Rose <karen.rose807@gmail.com>
2018-10-20 19:09:24 -0700

    Add background and text

--- client/js/mini-app/view.js
+++ client/js/mini-app/view.js
@@ -14,9 +14,14 @@ class MiniApp extends Component {
 	}
 
 	render() {
+		const { classes } = this.props
+
 		return (
 			<div>
-				The Mini App
+				<div className={classes.header}>
+					Made by women, for women to help you make independent, informed
+					healthcare decisions.
+				</div>
 				<MiniAppForm onSubmit={this.submit} />
 			</div>
 		)
@@ -24,8 +29,13 @@ class MiniApp extends Component {
 }
 
 const styles = {
-	root: {
-		height: '0px',
+	header: {
+		'background-color': '#016454',
+		color: '#abd3f9',
+		'font-family': 'sans-serif',
+		'font-size': '30px',
+		height: '104px',
+		padding: '60px 20px 40px 80px',
 	},
 }
 
