7161ca59dd4c9882269ff9fc48bb05d2ba07f450
Karen Rose <karen.rose807@gmail.com>
2018-10-21 13:50:17 -0700

    Format phone

--- client/js/mini-app/components/resource-card.js
+++ client/js/mini-app/components/resource-card.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 
+const formatPhoneNumber = phoneNumber => {
+	const areaCode = phoneNumber.substring(2, 5)
+	const prefix = phoneNumber.substring(5, 8)
+	const lineNumber = phoneNumber.substring(8, 12)
+
+	return `${areaCode}-${prefix}-${lineNumber}`
+}
+
 const ResourceCard = ({ classes, resource }) => {
 	const { line1, line2, city, state, zip } = resource.address
 
@@ -11,6 +19,11 @@ const ResourceCard = ({ classes, resource }) => {
 				{line1} <br />
 				{line2} {line2 && <br />}
 				{`${city}, ${state} ${zip}`}
+			</div>
+			<div className={classes.resourcePhone}>
+				<a href={`tel:${resource.phone}`}>
+					{formatPhoneNumber(resource.phone)}
+				</a>
 			</div>
 		</div>
 	)
@@ -28,6 +41,11 @@ const styles = {
 		'font-size': '20px',
 	},
 	resourceAddress: {
+		margin: '20px 0px 20px 30px',
+		'font-family': 'sans-serif',
+		'font-size': '15px',
+	},
+	resourcePhone: {
 		margin: '20px 0px 20px 30px',
 		'font-family': 'sans-serif',
 		'font-size': '15px',
