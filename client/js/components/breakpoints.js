d0b697b81bf2ed2f13ab4cda9242feb3202166c3
Karen Rose <karen.rose807@gmail.com>
2018-10-20 21:27:14 -0700

    Reorder constants

--- client/js/components/breakpoints.js
+++ client/js/components/breakpoints.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Responsive from 'react-responsive'
 
-const DESKTOP_MIN = 992
+const PHONE_MAX = 767
 const TABLET_MIN = 768
 const TABLET_MAX = 991
-const PHONE_MAX = 767
+const DESKTOP_MIN = 992
 
 const Desktop = props => <Responsive {...props} minWidth={DESKTOP_MIN} />
 const Tablet = props => (
