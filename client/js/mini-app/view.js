99676dd913a0ede105b0e0ccc03ea9062230b192
Karen Rose <karen.rose807@gmail.com>
2018-10-20 21:23:46 -0700

    Revert file

--- client/js/mini-app/view.js
+++ client/js/mini-app/view.js
@@ -5,7 +5,6 @@ import { withRouter } from 'react-router-dom'
 
 import MiniAppForm from './form'
 import { getPregnancyResourceCenters } from './data/action-creators'
-import { DesktopBreakpoint } from '../components/breakpoints'
 
 class MiniApp extends Component {
 	submit = ({ locationInput }) => {
@@ -15,15 +14,9 @@ class MiniApp extends Component {
 	}
 
 	render() {
-		const { classes } = this.props
-
 		return (
 			<div>
-				<div className={classes.header}>
-					Made by women, for women to help you make independent, informed
-					healthcare decisions.
-				</div>
-				<DesktopBreakpoint>DESKTOP!!!</DesktopBreakpoint>
+				The Mini App
 				<MiniAppForm onSubmit={this.submit} />
 			</div>
 		)
@@ -31,13 +24,8 @@ class MiniApp extends Component {
 }
 
 const styles = {
-	header: {
-		'background-color': '#016454',
-		color: '#abd3f9',
-		'font-family': 'sans-serif',
-		'font-size': '30px',
-		height: '104px',
-		padding: '60px 20px 40px 80px',
+	root: {
+		height: '0px',
 	},
 }
 
