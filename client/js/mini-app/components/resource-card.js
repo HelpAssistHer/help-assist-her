9ee3ebb027f45516de9fbd7987c8b2afb4b92b66
Karen Rose <karen.rose807@gmail.com>
2018-10-21 14:34:02 -0700

    Add background color

--- client/js/mini-app/components/resource-card.js
+++ client/js/mini-app/components/resource-card.js
@@ -13,32 +13,36 @@ const ResourceCard = ({ classes, resource }) => {
 	const { line1, line2, city, state, zip } = resource.address
 
 	return (
-		<div className={classes.root}>
-			<div className={classes.resourceName}>{resource.prcName}</div>
-			<div className={classes.resourceAddress}>
-				{line1} <br />
-				{line2} {line2 && <br />}
-				{`${city}, ${state} ${zip}`}
-			</div>
-			<div className={classes.resourcePhone}>
-				<a href={`tel:${resource.phone}`}>
-					{formatPhoneNumber(resource.phone)}
-				</a>
-			</div>
-			<div className={classes.resourceWebsite}>
-				<a href={resource.website} target="_blank" rel="noopener noreferrer">
-					{resource.website}
-				</a>
+		<div className={classes.resourceCardRoot}>
+			<div className={classes.resourceCardBorder}>
+				<div className={classes.resourceName}>{resource.prcName}</div>
+				<div className={classes.resourceAddress}>
+					{line1} <br />
+					{line2} {line2 && <br />}
+					{`${city}, ${state} ${zip}`}
+				</div>
+				<div className={classes.resourcePhone}>
+					<a href={`tel:${resource.phone}`}>
+						{formatPhoneNumber(resource.phone)}
+					</a>
+				</div>
+				<div className={classes.resourceWebsite}>
+					<a href={resource.website} target="_blank" rel="noopener noreferrer">
+						{resource.website}
+					</a>
+				</div>
 			</div>
 		</div>
 	)
 }
 
 const styles = {
-	root: {
+	resourceCardRoot: {
+		border: '20px solid rgb(93, 93, 93, .08)',
+	},
+	resourceCardBorder: {
 		border: '2px solid #3d65f9',
 		'border-radius': '4px',
-		margin: '20px 20px 20px 20px',
 	},
 	resourceName: {
 		margin: '40px 0px 0px 30px',
@@ -56,7 +60,7 @@ const styles = {
 		'font-size': '15px',
 	},
 	resourceWebsite: {
-		margin: '20px 0px 20px 30px',
+		margin: '20px 0px 40px 30px',
 		'font-family': 'sans-serif',
 		'font-size': '15px',
 	},
