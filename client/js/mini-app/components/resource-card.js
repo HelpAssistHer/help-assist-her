0000c09b573613852db073dc012894ee8453bb2f
Karen Rose <karen.rose807@gmail.com>
2018-10-29 14:42:21 -0700

    Add icons and style

--- client/js/mini-app/components/resource-card.js
+++ client/js/mini-app/components/resource-card.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 
+import LocationIcon from '../../components/icons/icon-components/location-icon'
+import PhoneIcon from '../../components/icons/icon-components/phone-icon'
+import GlobeIcon from '../../components/icons/icon-components/globe-icon'
+
 const formatPhoneNumber = phoneNumber => {
 	const areaCode = phoneNumber.substring(2, 5)
 	const prefix = phoneNumber.substring(5, 8)
@@ -16,18 +20,31 @@ const ResourceCard = ({ classes, resource }) => {
 		<div className={classes.resourceCardRoot}>
 			<div className={classes.resourceCardBorder}>
 				<div className={classes.resourceName}>{resource.prcName}</div>
+
 				<div className={classes.resourceAddress}>
-					{line1} <br />
-					{line2} {line2 && <br />}
-					{`${city}, ${state} ${zip}`}
+					<LocationIcon height={32} width={32} />
+					<div className={classes.margin}>
+						{line1} <br />
+						{line2} {line2 && <br />}
+						{`${city}, ${state} ${zip}`}
+					</div>
 				</div>
+
 				<div className={classes.resourcePhone}>
-					<a href={`tel:${resource.phone}`}>
+					<PhoneIcon height={32} width={32} />
+					<a className={classes.margin} href={`tel:${resource.phone}`}>
 						{formatPhoneNumber(resource.phone)}
 					</a>
 				</div>
+
 				<div className={classes.resourceWebsite}>
-					<a href={resource.website} target="_blank" rel="noopener noreferrer">
+					<GlobeIcon height={32} width={32} />
+					<a
+						className={classes.margin}
+						href={resource.website}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
 						{resource.website}
 					</a>
 				</div>
@@ -49,16 +66,25 @@ const styles = {
 		'font-size': '20px',
 	},
 	resourceAddress: {
+		display: 'flex',
+		'align-items': 'flex-start',
 		margin: '20px 0px 20px 30px',
 		'font-size': '15px',
 	},
 	resourcePhone: {
+		display: 'flex',
+		'align-items': 'center',
 		margin: '20px 0px 20px 30px',
 		'font-size': '15px',
 	},
 	resourceWebsite: {
+		display: 'flex',
+		'align-items': 'center',
 		margin: '20px 0px 40px 30px',
 		'font-size': '15px',
+	},
+	margin: {
+		'margin-left': '13px',
 	},
 }
 
