9092db4c694cd4eb26abcdbee6a527bb61cac93c
Karen Rose <karen.rose807@gmail.com>
2018-10-29 18:21:02 -0700

    Revert change

--- client/js/mini-app/header.js
+++ client/js/mini-app/header.js
@@ -10,10 +10,6 @@ const Header = ({ classes }) => {
 
 	return (
 		<div>
-			<div className={classes.light}>HAH is the best!</div>
-			<div className={classes.regular}>HAH is the best!</div>
-			<div className={classes.medium}>HAH is the best!</div>
-			<div className={classes.bold}>HAH is the best!</div>
 			<Phone>
 				<div className={classes.headerPhone}>{headerText}</div>
 			</Phone>
@@ -25,18 +21,6 @@ const Header = ({ classes }) => {
 }
 
 const styles = {
-	light: {
-		'font-family': 'hah-light',
-	},
-	regular: {
-		'font-family': 'hah-regular',
-	},
-	medium: {
-		'font-family': 'hah-medium',
-	},
-	bold: {
-		'font-family': 'hah-bold',
-	},
 	headerPhone: {
 		'background-color': '#016454',
 		color: '#abd3f9',
