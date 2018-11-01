7d2469b5b31385bccac042a5120bde093aa0cb98
Karen Rose <karen.rose807@gmail.com>
2018-10-31 23:43:28 -0700

    Style desktop version

--- client/js/mini-app/components/go-button.js
+++ client/js/mini-app/components/go-button.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import injectSheet from 'react-jss'
+import classnames from 'classnames'
+
+import { Phone, Desktop } from '../../components/breakpoints'
 
 const GoButton = ({ classes, onClick }) => {
+	const buttonText = 'GO'
+
 	return (
-		<div className={classes.goButtonRoot}>
-			<button className={classes.button} type="submit" onClick={onClick}>
-				GO
-			</button>
+		<div>
+			<div className={classes.goButtonRoot}>
+				<Phone>
+					<button
+						className={classnames(classes.buttonCommon, classes.buttonPhone)}
+						type="submit"
+						onClick={onClick}
+					>
+						{buttonText}
+					</button>
+				</Phone>
+
+				<Desktop>
+					<button
+						className={classnames(classes.buttonCommon, classes.buttonDesktop)}
+						type="submit"
+						onClick={onClick}
+					>
+						{buttonText}
+					</button>
+				</Desktop>
+			</div>
 		</div>
 	)
 }
@@ -16,23 +39,32 @@ const styles = {
 		display: 'flex',
 		'justify-content': 'center',
 	},
-	button: {
-		border: '1px solid #3D65F9',
-		'border-radius': '8px',
+	buttonCommon: {
 		'background-color': '#FFFFFF',
+		'border-radius': '8px',
 		color: '#000000',
-		'font-family': 'hah',
+		'font-family': 'hah', // update
 		'text-align': 'center',
 		cursor: 'pointer',
 		'text-decoration': 'none',
-		'font-size': '14px',
-		height: '31px',
-		width: '96px',
 		outline: 'none',
 		'&:hover': {
 			'background-color': '#3D65F9',
 			color: '#FFFFFF',
 		},
+	},
+	buttonPhone: {
+		border: '1px solid #3D65F9',
+		'font-size': '14px',
+		height: '31px',
+		width: '96px',
+	},
+	buttonDesktop: {
+		border: '2px solid #3D65F9',
+		'font-size': '24px',
+		'letter-spacing': '0.5px',
+		height: '54px',
+		width: '186px',
 	},
 }
 
