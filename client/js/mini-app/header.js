90ac530cfebd29b143cfbb46151bff740c04954e
Karen Rose <karen.rose807@gmail.com>
2018-10-31 20:43:45 -0700

    Get working for desktop and phone at the same time

--- client/js/mini-app/header.js
+++ client/js/mini-app/header.js
@@ -13,80 +13,82 @@ const Header = ({ classes }) => {
 	return (
 		<div>
 			<Spacer height="100px" />
-			<div className={classes.headerContainer}>
-				<div className={classes.headerText}>
-					<div className={classes.titleText}>{titleText}</div>
-					<Spacer height="40px" />
-					<div className={classes.descriptionText}>{descriptionText}</div>
+
+			<Phone>
+				<div className={classes.headerContainerPhone}>
+					<div className={classes.headerText}>
+						<div className={classes.titleTextPhone}>{titleText}</div>
+						<Spacer height="40px" />
+						<div className={classes.descriptionTextPhone}>
+							{descriptionText}
+						</div>
+					</div>
+
+					<img
+						src="../img/two-women-hugging.jpg"
+						alt="Two Close Friends"
+						width={500}
+					/>
 				</div>
+			</Phone>
 
-				<img
-					src="../img/two-women-hugging.jpg"
-					alt="Two Close Friends"
-					width={500}
-				/>
-			</div>
+			<Desktop>
+				<div className={classes.headerContainerDesktop}>
+					<div className={classes.headerText}>
+						<div className={classes.titleTextDesktop}>{titleText}</div>
+						<Spacer height="40px" />
+						<div className={classes.descriptionTextDesktop}>
+							{descriptionText}
+						</div>
+					</div>
 
-			{/*<Spacer height="100px" />*/}
-			{/*<div className={classes.headerContainer}>*/}
-			{/*<div className={classes.headerText}>*/}
-			{/*<div className={classes.titleText}>{titleText}</div>*/}
-			{/*<Spacer height="40px" />*/}
-			{/*<div className={classes.descriptionText}>{descriptionText}</div>*/}
-			{/*</div>*/}
-
-			{/*<img*/}
-			{/*src="../img/two-women-hugging.jpg"*/}
-			{/*alt="Two Close Friends"*/}
-			{/*height="455"*/}
-			{/*/>*/}
-			{/*</div>*/}
+					<img
+						src="../img/two-women-hugging.jpg"
+						alt="Two Close Friends"
+						height={455}
+					/>
+				</div>
+			</Desktop>
 		</div>
 	)
 }
 
 const styles = {
-	headerContainer: {
+	headerContainerPhone: {
 		display: 'flex',
 		'flex-direction': 'column',
 		'background-color': '#016454',
 		width: '500px',
 	},
+	headerContainerDesktop: {
+		display: 'flex',
+		'background-color': '#016454',
+		height: '455px',
+	},
 	headerText: {
 		flex: '1 1 0',
 		color: '#abd3f9',
 		'font-family': 'hah-regular',
-		'text-align': 'center',
+		'text-align': 'center', // ? for desktop
 	},
-	titleText: {
+	titleTextPhone: {
 		padding: '74px 80px 0px 80px',
 		'font-size': '40px',
 		'font-weight': 300, // fix
 		'letter-spacing': '-0.5px',
 	},
-	descriptionText: {
+	titleTextDesktop: {
+		padding: '74px 80px 0px 80px',
+		'font-size': '80px',
+	},
+	descriptionTextPhone: {
 		padding: '0px 80px 132px 80px',
 		'font-size': '18px',
 	},
-	// headerContainer: {
-	// 	display: 'flex',
-	// 	'background-color': '#016454',
-	// 	height: '455px',
-	// },
-	// headerText: {
-	// 	flex: '1 1 0',
-	// 	color: '#abd3f9',
-	// 	'font-family': 'hah-regular',
-	// 	'text-align': 'center', // ?
-	// },
-	// titleText: {
-	// 	padding: '74px 80px 0px 80px',
-	// 	'font-size': '80px',
-	// },
-	// descriptionText: {
-	// 	padding: '0px 80px 132px 80px',
-	// 	'font-size': '24px',
-	// },
+	descriptionTextDesktop: {
+		padding: '0px 80px 132px 80px',
+		'font-size': '24px',
+	},
 }
 
 export default injectSheet(styles)(Header)
