11ecbcc14c494dbd51853cfb52295e6120897523
Karen Rose <karen.rose807@gmail.com>
2018-10-31 20:26:15 -0700

    Add image and update desktop style

--- client/js/mini-app/header.js
+++ client/js/mini-app/header.js
@@ -5,63 +5,99 @@ import { Phone, Desktop } from '../components/breakpoints'
 import Spacer from '../components/spacer'
 
 const Header = ({ classes }) => {
-	const titleText = 'By Women. For Women.'
+	const titleText = 'We have your back.'
 
 	const descriptionText =
-		'Made by women, for women to help you make independent, ' +
-		'informed healthcare decisions.'
+		'We’re here to help you find the local resources you need.'
 
 	return (
 		<div>
-			<Phone>
-				<div>
-					<div className={classes.imagePhone}>Image</div>
-					<div className={classes.headerPhone}>
-						<div className={classes.titlePhone}>{titleText}</div>
-						<Spacer height="12px" />
-						<div>{descriptionText}</div>
-					</div>
+			<Spacer height="100px" />
+			<div className={classes.headerContainer}>
+				<div className={classes.headerText}>
+					<div className={classes.titleText}>{titleText}</div>
+					<Spacer height="40px" />
+					<div className={classes.descriptionText}>{descriptionText}</div>
 				</div>
-			</Phone>
 
-			<Desktop>
-				<div className={classes.headerDesktop}>{descriptionText}</div>
-			</Desktop>
+				<img
+					src="../img/two-women-hugging.jpg"
+					alt="Two Close Friends"
+					height="455"
+				/>
+			</div>
+
+			{/*width="500" height="455"*/}
+			{/*<Phone>*/}
+			{/*<div>*/}
+			{/*<div className={classes.imagePhone}>Image</div>*/}
+			{/*<div className={classes.headerPhone}>*/}
+			{/*<div className={classes.titlePhone}>{titleText}</div>*/}
+			{/*<Spacer height="12px" />*/}
+			{/*<div>{descriptionText}</div>*/}
+			{/*</div>*/}
+			{/*</div>*/}
+			{/*</Phone>*/}
+
+			{/*<Desktop>*/}
+			{/**/}
+			{/*</Desktop>*/}
 		</div>
 	)
 }
 
 const styles = {
-	imagePhone: {
-		height: '264px',
-	},
-	headerPhone: {
+	headerContainer: {
 		display: 'flex',
-		'flex-direction': 'column',
-		'justify-content': 'center',
 		'background-color': '#016454',
+		height: '455px',
+	},
+	headerText: {
+		flex: '1 1 0',
 		color: '#abd3f9',
-		height: '255px',
-		padding: '0px 80px 0px 80px',
+		'font-family': 'hah-regular',
 		'text-align': 'center',
 	},
-	titlePhone: {
-		'font-family': 'hah',
-		'font-size': '40px',
-		'font-weight': 300,
-		'letter-spacing': '-0.5px',
+	titleText: {
+		padding: '74px 80px 0px 80px',
+		'font-size': '80px',
 	},
-	descriptionTextPhone: {
-		'font-size': '18px',
+	descriptionText: {
+		padding: '0px 80px 132px 80px',
+		'font-size': '24px',
 	},
-	headerDesktop: {
-		'background-color': '#016454',
-		color: '#abd3f9',
-		'font-size': '30px',
-		height: '104px',
-		padding: '60px 20px 40px 80px',
-		'text-shadow': '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
-	},
+	// headerPhone: {
+	// 	display: 'flex',
+	// 	'flex-direction': 'column',
+	// 	'justify-content': 'center',
+	// 	'background-color': '#016454',
+	// 	color: '#abd3f9',
+	// 	height: '255px',
+	// 	padding: '0px 80px 0px 80px',
+	// 	'text-align': 'center',
+	// },
+	// titlePhone: {
+	// 	'font-family': 'hah',
+	// 	'font-size': '40px',
+	// 	'font-weight': 300,
+	// 	'letter-spacing': '-0.5px',
+	// },
+	// titleDesktop: {
+	//
+	// },
+	// descriptionTextPhone: {
+	// 	'font-size': '18px',
+	// },
+	// headerDesktop: {
+	// 	// 'flex-basis': '50%',
+	// 	flex: '1 1 0',
+	// 	'background-color': '#016454',
+	// 	color: '#abd3f9',
+	// 	'font-size': '30px',
+	// 	// height: '104px',
+	// 	padding: '60px 20px 40px 80px',
+	// 	'text-shadow': '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+	// },
 }
 
 export default injectSheet(styles)(Header)
