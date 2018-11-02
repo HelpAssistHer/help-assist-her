d9dbd67b0ba4b15ba080504cc704bfc62a776a5d
Karen Rose <karen.rose807@gmail.com>
2018-11-02 10:57:58 -0700

    Add social media component

--- client/js/mini-app/components/footer.js
+++ client/js/mini-app/components/footer.js
@@ -4,29 +4,30 @@ import injectSheet from 'react-jss'
 
 import Spacer from '../../components/spacer'
 import LogoBetaWhite from '../../components/icons/icon-components/logo-beta-white'
+import SocialMedia from './social-media'
 
-const MiniAppFooter = ({ classes }) => {
-	return (
-		<div>
-			<Spacer height="100px" />
-			<div className={classes.footerRoot}>
-				<LogoBetaWhite height={25} width={160} />
-				<Link to="/about" className={classes.navigationLink}>
-					About
-				</Link>
-				<Link to="/feedback" className={classes.navigationLink}>
-					Feedback
-				</Link>
-				<div className={classes.copyrightText}>&copy; Help Assist Her 2018</div>
-			</div>
+const MiniAppFooter = ({ classes }) => (
+	<div>
+		<Spacer height="87px" />
+		<div className={classes.footerRoot}>
+			<LogoBetaWhite height={25} width={160} />
+			<Link to="/about" className={classes.navigationLink}>
+				About
+			</Link>
+			<Link to="/feedback" className={classes.navigationLink}>
+				Feedback
+			</Link>
+			<div className={classes.copyrightText}>&copy; Help Assist Her 2018</div>
+			<SocialMedia />
 		</div>
-	)
-}
+	</div>
+)
 
 const styles = {
 	footerRoot: {
 		height: '149px',
 		'background-color': '#3D65F9',
+		padding: '35px 82px 0px 44px',
 	},
 	navigationLink: {
 		'font-family': 'hah-regular',
