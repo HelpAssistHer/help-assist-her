ccb105cef5702233e05f7394920483a647a7393b
Karen Rose <karen.rose807@gmail.com>
2018-11-02 14:42:38 -0700

    Format input for desktop

--- client/js/mini-app/components/input.js
+++ client/js/mini-app/components/input.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 
+import { Phone, Desktop } from '../../components/breakpoints'
+
 const Input = ({ classes, input, type, placeholder }) => {
 	return (
 		<div className={classes.inputRoot}>
-			<div className={classes.border}>
-				<input
-					className={classes.inputPhone}
-					type={type}
-					placeholder={placeholder}
-					{...input}
-				/>
-			</div>
+			<Phone>
+				<div className={classes.borderPhone}>
+					<input
+						className={classes.inputPhone}
+						type={type}
+						placeholder={placeholder}
+						{...input}
+					/>
+				</div>
+			</Phone>
+
+			<Desktop>
+				<div className={classes.borderDesktop}>
+					<input
+						className={classes.inputDesktop}
+						type={type}
+						placeholder={placeholder}
+						{...input}
+					/>
+				</div>
+			</Desktop>
 		</div>
 	)
 }
@@ -21,9 +36,13 @@ const styles = {
 		display: 'flex',
 		'justify-content': 'center',
 	},
-	border: {
+	borderPhone: {
 		'border-bottom': '1px solid #979797',
 		width: '250px',
+	},
+	borderDesktop: {
+		'border-bottom': '2px solid #000000',
+		width: '478px',
 	},
 	inputPhone: {
 		border: 'none',
@@ -36,6 +55,19 @@ const styles = {
 		'line-height': '16px',
 		'text-align': 'center',
 		height: '26px',
+		width: '100%',
+	},
+	inputDesktop: {
+		border: 'none',
+		outline: 'none',
+		'font-family': 'hah-light',
+		'font-size': '24px',
+		color: '#000000',
+		opacity: '0.24',
+		'letter-spacing': '0.5px',
+		'line-height': '30px',
+		'text-align': 'center',
+		height: '43px',
 		width: '100%',
 	},
 }
