5eb680012b05788f1693fe245ca297cf88fa3776
Karen Rose <karen.rose807@gmail.com>
2018-11-01 19:09:57 -0700

    Add logo style

--- client/js/mini-app/header.js
+++ client/js/mini-app/header.js
@@ -3,6 +3,7 @@ import injectSheet from 'react-jss'
 
 import { Phone, Desktop } from '../components/breakpoints'
 import Spacer from '../components/spacer'
+import LogoBetaBlack from '../components/icons/icon-components/logo-beta-black'
 
 const Header = ({ classes }) => {
 	const titleText = 'We have your back.'
@@ -12,41 +13,51 @@ const Header = ({ classes }) => {
 
 	return (
 		<div>
-			<Spacer height="100px" />
-
 			<Phone>
-				<div className={classes.headerContainerPhone}>
-					<img
-						className={classes.imagePhone}
-						src="../img/two-women-hugging.jpg"
-						alt="Two Close Friends"
-					/>
+				<div>
+					<div className={classes.logoPhone}>
+						<LogoBetaBlack height={25} width={160} />
+					</div>
 
-					<div className={classes.headerTextPhone}>
-						<div className={classes.titleTextPhone}>{titleText}</div>
-						<Spacer height="40px" />
-						<div className={classes.descriptionTextPhone}>
-							{descriptionText}
+					<div className={classes.headerContainerPhone}>
+						<img
+							className={classes.imagePhone}
+							src="../img/two-women-hugging.jpg"
+							alt="Two Close Friends"
+						/>
+
+						<div className={classes.headerTextPhone}>
+							<div className={classes.titleTextPhone}>{titleText}</div>
+							<Spacer height="40px" />
+							<div className={classes.descriptionTextPhone}>
+								{descriptionText}
+							</div>
 						</div>
 					</div>
 				</div>
 			</Phone>
 
 			<Desktop>
-				<div className={classes.headerContainerDesktop}>
-					<div className={classes.headerTextDesktop}>
-						<div className={classes.titleTextDesktop}>{titleText}</div>
-						<Spacer height="40px" />
-						<div className={classes.descriptionTextDesktop}>
-							{descriptionText}
-						</div>
+				<div>
+					<div className={classes.logoDesktop}>
+						<LogoBetaBlack height={33} width={208} />
 					</div>
 
-					<img
-						className={classes.imageDesktop}
-						src="../img/two-women-hugging.jpg"
-						alt="Two Close Friends"
-					/>
+					<div className={classes.headerContainerDesktop}>
+						<div className={classes.headerTextDesktop}>
+							<div className={classes.titleTextDesktop}>{titleText}</div>
+							<Spacer height="40px" />
+							<div className={classes.descriptionTextDesktop}>
+								{descriptionText}
+							</div>
+						</div>
+
+						<img
+							className={classes.imageDesktop}
+							src="../img/two-women-hugging.jpg"
+							alt="Two Close Friends"
+						/>
+					</div>
 				</div>
 			</Desktop>
 		</div>
@@ -54,6 +65,18 @@ const Header = ({ classes }) => {
 }
 
 const styles = {
+	logoPhone: {
+		height: '55px',
+		margin: '0px 0px 0px 17px',
+		display: 'flex',
+		'align-items': 'center',
+	},
+	logoDesktop: {
+		height: '100px',
+		margin: '0px 0px 0px 42px',
+		display: 'flex',
+		'align-items': 'center',
+	},
 	headerContainerPhone: {
 		display: 'flex',
 		'flex-direction': 'column',
