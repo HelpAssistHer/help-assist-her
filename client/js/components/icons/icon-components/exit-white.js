8cad3f931cac5ea902b37e13eef0c1728fe7ddb1
Karen Rose <karen.rose807@gmail.com>
2018-11-01 22:40:08 -0700

    Add exit button

---
+++ client/js/components/icons/icon-components/exit-white.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function ExitWhite(props) {
+	return (
+		<svg
+			width="16px"
+			height="16px"
+			viewBox="0 0 16 16"
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			{...props}
+		>
+			{/* Generator: Sketch 46.2 (44496) - http://www.bohemiancoding.com/sketch */}
+			<title>Exit_White_Mobile</title>
+			<desc>Created with Sketch.</desc>
+			<defs />
+			<g
+				id="Symbols"
+				stroke="none"
+				strokeWidth={1}
+				fill="none"
+				fillRule="evenodd"
+				strokeLinecap="square"
+			>
+				<g id="Exit_White_Mobile" stroke="#FFFFFF" strokeWidth={2}>
+					<path d="M0.375,0.5 L15.4479995,15.5729995" id="Line-2" />
+					<path
+						d="M0.375,0.5 L15.4479995,15.5729995"
+						id="Line-2-Copy-3"
+						transform="translate(8.000000, 8.000000) scale(-1, 1) translate(-8.000000, -8.000000) "
+					/>
+				</g>
+			</g>
+		</svg>
+	)
+}
--- client/js/mini-app/components/menu.js
+++ client/js/mini-app/components/menu.js
@@ -4,30 +4,45 @@ import { Link } from 'react-router-dom'
 
 import Spacer from '../../components/spacer'
 import LogoBetaWhite from '../../components/icons/icon-components/logo-beta-white'
+import ExitWhite from '../../components/icons/icon-components/exit-white'
 
 const Menu = ({ classes }) => {
 	return (
 		<div className={classes.menu}>
-			<LogoBetaWhite height={25} width={160} />
-			<Link to="/about" className={classes.navigationLinkPhone}>
-				About
-			</Link>
-			<Spacer height="32px" />
-			<Link to="/feedback" className={classes.navigationLinkPhone}>
-				Feedback
-			</Link>
+			<div className={classes.logoAndExit}>
+				<LogoBetaWhite height={25} width={160} />
+				<ExitWhite height={16} width={16} />
+			</div>
+
+			<div className={classes.links}>
+				<Link to="/about" className={classes.navigationLinkPhone}>
+					About
+				</Link>
+				<Spacer height="32px" />
+				<Link to="/feedback" className={classes.navigationLinkPhone}>
+					Feedback
+				</Link>
+			</div>
 		</div>
 	)
 }
 
 const styles = {
 	menu: {
-		display: 'flex',
-		'flex-direction': 'column',
 		position: 'fixed',
 		width: '100%',
 		height: '100%',
 		'background-color': '#3D65F9',
+	},
+	logoAndExit: {
+		display: 'flex',
+		'justify-content': 'space-between',
+		'border-bottom': '1px solid #FFFFFF',
+		padding: '20px 25px 20px 20px',
+	},
+	links: {
+		display: 'flex',
+		'flex-direction': 'column',
 		padding: '45px 0px 0px 24px',
 	},
 	navigationLinkPhone: {
