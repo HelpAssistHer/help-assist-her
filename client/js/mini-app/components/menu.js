adcdb7797404c493add847902d5e20e9e17e99a3
Karen Rose <karen.rose807@gmail.com>
2018-11-02 09:32:33 -0700

    Add social media links

--- client/js/mini-app/components/menu.js
+++ client/js/mini-app/components/menu.js
@@ -10,6 +10,10 @@ import FacebookWhite from '../../components/icons/icon-components/facebook-white
 import TwitterWhite from '../../components/icons/icon-components/twitter-white'
 import InstagramWhite from '../../components/icons/icon-components/instagram-white'
 
+const HAH_FACEBOOK_URL = 'https://www.facebook.com/HelpAssistHer/'
+const HAH_TWITTER_URL = 'https://twitter.com/helpassisther'
+const HAH_INSTAGRAM_URL = 'https://www.instagram.com/helpassisther/'
+
 const MenuWithStyle = ({ classes, history }) => (
 	<div className={classes.menu}>
 		<div className={classes.logoAndExit}>
@@ -30,11 +34,19 @@ const MenuWithStyle = ({ classes, history }) => (
 		</div>
 
 		<div className={classes.socialMedia}>
-			<FacebookWhite height={41} width={41} />
+			<a href={HAH_FACEBOOK_URL} target="_blank" rel="noopener noreferrer">
+				<FacebookWhite height={41} width={41} />
+			</a>
 			<Spacer width="14px" />
-			<TwitterWhite height={41} width={41} />
+
+			<a href={HAH_TWITTER_URL} target="_blank" rel="noopener noreferrer">
+				<TwitterWhite height={41} width={41} />
+			</a>
 			<Spacer width="14px" />
-			<InstagramWhite height={41} width={41} />
+
+			<a href={HAH_INSTAGRAM_URL} target="_blank" rel="noopener noreferrer">
+				<InstagramWhite height={41} width={41} />
+			</a>
 		</div>
 	</div>
 )
