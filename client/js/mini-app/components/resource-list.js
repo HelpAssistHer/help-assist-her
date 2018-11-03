4a4981769409c3281a8f1988686c00f0c1b5bbe0
Karen Rose <karen.rose807@gmail.com>
2018-11-02 17:10:48 -0700

    test commit

--- client/js/mini-app/components/resource-list.js
+++ client/js/mini-app/components/resource-list.js
@@ -5,6 +5,12 @@ import injectSheet from 'react-jss'
 
 import ResourceCard from './resource-card'
 import LogoAndNavigation from '../logo-and-navigation'
+import NoResults from './no-results'
+import Footer from './footer'
+
+const searchResultsMessage =
+	'Your search results have been arranged by closest distance to your location data.'
+const noResultsMessage = 'No results match your search.'
 
 const mapStateToProps = state => {
 	return {
@@ -13,23 +19,62 @@ const mapStateToProps = state => {
 }
 
 const ResourceListView = ({ classes, pregnancyResourceCenters }) => {
-	return (
-		<div>
+	// const containerView = (
+	// 	<div>
+	// 		<LogoAndNavigation />
+	// 		<div className={classes.header}>
+	// 			Your search results have been arranged by closest distance to your
+	// 			location data.
+	// 		</div>
+	// 	</div>
+	// )
+
+	const noResultsView = (
+		<div className={classes.noResultsRoot}>
 			<LogoAndNavigation />
-			<div className={classes.header}>
-				Your search results have been arranged by closest distance to your
-				location data.
+			<div className={classes.header}>{searchResultsMessage}</div>
+			<div className={classes.noResults}>
+				{noResultsMessage}
+				<button>Return to Search</button>
 			</div>
-			<div className={classes.root}>
-				{_.map(pregnancyResourceCenters, prc => {
-					return <ResourceCard key={prc._id} resource={prc} />
-				})}
+			<div className={classes.footer}>
+				<Footer />
 			</div>
 		</div>
 	)
+
+	return noResultsView
+
+	// const noResults = _.get(pregnancyResourceCenters, 'statusCode') === 404
+	// if (noResults) return <NoResults />
+
+	// return (
+	// 	<div>
+	// 		<LogoAndNavigation />
+	// 		<div className={classes.header}>
+	// 			Your search results have been arranged by closest distance to your
+	// 			location data.
+	// 		</div>
+	// 		<div>
+	// 			{_.map(pregnancyResourceCenters, prc => {
+	// 				return <ResourceCard key={prc._id} resource={prc} />
+	// 			})}
+	// 		</div>
+	// 	</div>
+	// )
 }
 
 const styles = {
+	noResultsRoot: {
+		height: '100%',
+		display: 'flex',
+		'flex-direction': 'column',
+		'justify-content': 'space-between',
+	},
+	noResults: {
+		'background-color': 'rgba(93,93,93,0.08)',
+		// border: 3px solid #3D65F9;
+	},
 	header: {
 		padding: '30px 0px 30px 0px',
 		'background-color': '#3d65f9',
@@ -37,6 +82,9 @@ const styles = {
 		color: '#99cccc',
 		'text-align': 'center',
 	},
+	// footer: {
+	// 	'align-self': 'flex-end',
+	// },
 }
 
 const ResourceList = connect(mapStateToProps)(ResourceListView)
