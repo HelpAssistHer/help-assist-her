8fe753eb56e03ab153ed4e82438982157157f1c8
Bhavya Bharti <bhavy@ramank-home.redmond.corp.microsoft.com>
2018-11-13 15:27:54 -0800

    form reset works fine

--- client/js/verification-portal/pregnancy-resource-center/form.js
+++ client/js/verification-portal/pregnancy-resource-center/form.js
@@ -240,34 +240,32 @@ class VerificationPortalFormClass extends Component {
 					{/*</div>*/}
 					{/*</div>*/}
 
-					{
-						// <Spacer height="81px" />
-						//
-						// <div className={classes.gridField}>
-						// 	<div className={classes.headingGrid}>
-						// 		<Heading text="SERVICES" size="medium" />
-						// 	</div>
-						// </div>
-						// <Spacer height="53px" />
-						// <Services />
-					}
+					<Spacer height="81px" />
+
+					<div className={classes.gridField}>
+						<div className={classes.headingGrid}>
+							<Heading text="SERVICES" size="medium" />
+						</div>
+					</div>
+					<Spacer height="53px" />
+					<Services />
 				</div>
-				{
-					// <div className={classes.formSection}>
-					// 	<div>
-					// 		<Field
-					// 			name="otherServices"
-					// 			component="textarea"
-					// 			placeholder="Add additional services not listed above here..."
-					// 			rows="4"
-					// 			cols="50"
-					// 		/>
-					// 	</div>
-					// 	<div className={classes.parent}>
-					// 		<Field name="verifiedData.services.verified" component={Toggle} />
-					// 	</div>
-					// </div>
-				}
+
+				<div className={classes.formSection}>
+					<div>
+						<Field
+							name="otherServices"
+							component="textarea"
+							placeholder="Add additional services not listed above here..."
+							rows="4"
+							cols="50"
+						/>
+					</div>
+					<div className={classes.parent}>
+						<Field name="verifiedData.services.verified" component={Toggle} />
+					</div>
+				</div>
+
 				<div className={classes.formSection}>
 					<Heading text="HOURS" size="medium" />
 					<Spacer height="50px" />
