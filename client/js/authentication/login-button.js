3f6623f17f613ce082321c6c2a6e83d300648442
Bhavya Bharti <bhavyab@outlook.com>
2019-01-15 20:06:44 -0800

    logo addition with text on home screen (#139)
    * backgroung image set
    
    * verification portal backroung color set to white
    
    * logo addition with text on home screen
    
    * backroung image is contained in verification app

--- client/js/authentication/login-button.js
+++ client/js/authentication/login-button.js
@@ -15,7 +15,7 @@ const styles = {
 		cursor: 'pointer',
 		height: '65px',
 		width: '30%',
-		margin: '25% 35%',
+		margin: '35%',
 		'border-radius': '32px',
 		background:
 			'rgb(215, 97, 127) none repeat scroll 0% 0% / auto padding-box border-box',
--- client/js/hah-app/landing.js
+++ client/js/hah-app/landing.js
@@ -11,6 +11,19 @@ class Landing extends React.Component {
 		return (
 			<div className={classes.landing}>
 				<Link to="/verification">Verification Portal</Link>
+				<ul className={classes.homeScreen}>
+					<li>
+						<img
+							src="./img/logo-white.png"
+							alt="Help Assist Her logo"
+							className={classes.logoWhite}
+						/>
+					</li>
+					<li className={classes.textOne}>Information Database</li>
+					<li className={classes.textTwo}>
+						Sign in to start helping our sister out!
+					</li>
+				</ul>
 				<LoginButton />
 			</div>
 		)
@@ -25,6 +38,33 @@ const styles = {
 		'background-position': 'center',
 		'background-repeat': 'no-repeat',
 		'background-size': 'cover',
+	},
+	homeScreen: {
+		position: 'absolute',
+		top: '20%',
+		margin: '0%',
+		padding: '0%',
+		width: '100%',
+		'list-style': 'none',
+		'text-align': 'center',
+		color: '#FFFFFF',
+		'font-family': 'Century Gothic',
+		'font-weight': 'bold',
+	},
+	logoWhite: {
+		height: '67.07px',
+		width: '338.23px',
+	},
+	textOne: {
+		'font-size': '72px',
+		'letter-spacing': '3px',
+		'line-height': '88px',
+	},
+	textTwo: {
+		'font-size': '24px',
+		'letter-spacing': '0.33px',
+		'line-height': '29px',
+		'margin-top': '45px',
 	},
 }
 const LandingWithStyle = injectSheet(styles)(Landing)
