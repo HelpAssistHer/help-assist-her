e7df5607eadf92601d4f30cf016c90fba061093c
Valery Zajkov <wuvalzaj@gmail.com>
2019-01-22 22:08:51 -0500

    FB login button (#137)
    * Remove login button from form page, add login button to landing page
    
    * Fix lint errors

--- client/js/authentication/login-button.js
+++ client/js/authentication/login-button.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 import Spacer from '../components/spacer'
+import FacebookLoginButtonContainer from './facebook-login-button/index'
 
 const LoginButton = ({ classes }) => (
-	<div>
+	<div className={classes.button}>
 		<Spacer height="10px" />
-		<button className={classes.button}>Sign in with your Facebook</button>
+		<FacebookLoginButtonContainer />
 	</div>
 )
 
 const styles = {
 	button: {
 		color: 'rgb(255, 255, 255)',
-		cursor: 'pointer',
-		height: '65px',
 		width: '30%',
 		margin: '35%',
 		'border-radius': '32px',
-		background:
-			'rgb(215, 97, 127) none repeat scroll 0% 0% / auto padding-box border-box',
-		'font-family': 'san-serif',
-		'font-size': '25px !important',
+		'font-family': 'sans-serif',
 	},
 }
 
