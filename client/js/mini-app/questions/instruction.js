b724a65df2aeecec593b742a734c79348d0a791f
Karen Rose <karen.rose807@gmail.com>
2019-02-09 18:53:09 -0800

    Fix CSS for instructions

--- client/js/mini-app/questions/instruction.js
+++ client/js/mini-app/questions/instruction.js
@@ -36,20 +36,30 @@ const styles = {
 		'letter-spacing': '2px',
 		'font-size': '10px',
 		color: '#3d65f9',
+		height: '20px',
+		'line-height': '20px',
 	},
 	stepDescriptionPhone: {
+		'font-family': 'hah-regular',
 		color: '#000000',
 		'font-size': '14px',
+		height: '20px',
+		'line-height': '20px',
 	},
 	stepNumberDesktop: {
 		'font-family': 'hah-bold',
-		'letter-spacing': '2px',
+		'letter-spacing': '5px',
 		'font-size': '18px',
 		color: '#3d65f9',
+		height: '40px',
+		'line-height': '40px',
 	},
 	stepDescriptionDesktop: {
+		'font-family': 'hah-regular',
 		color: '#000000',
 		'font-size': '25px',
+		height: '40px',
+		'line-height': '40px',
 	},
 }
 
