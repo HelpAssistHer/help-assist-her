cdc9d0fe126d6dd6d8dab3ebdedba376a253677e
Kate Sills <katelynsills@gmail.com>
2019-02-16 20:04:52 -0800

    add in geocode update from master

--- server/data-import/geocode.js
+++ server/data-import/geocode.js
@@ -12,11 +12,7 @@ const {
 	getLocation,
 	addLocation,
 	saveDoc,
-<<<<<<< HEAD
 	getSafeFullAddress,
-=======
-	getFullAddress,
->>>>>>> rewrite helpers
 } = require('../util/geocode-helpers')
 
 const { safePipeP } = require('../util/ramda-util')
@@ -33,8 +29,6 @@ mongoose.connect(config.mongo.connectionString).then(
 	},
 )
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const geocodeAndSave = async pregnancyCenter => {
 	try {
 		return safePipeP([
@@ -43,72 +37,6 @@ const geocodeAndSave = async pregnancyCenter => {
 			getLocation,
 			R.partial(addLocation, [pregnancyCenter]),
 			saveDoc,
-=======
-// ramda examples
-
-// const then = (f, p) => p.then(f)
-// const pipeP = R.pipeWith(then)
-// const safePipe = R.pipeWith((f, res) => R.isNil(res) || R.isEmpty(res) ? res : f(res))
-
-const safePipeP = R.pipeWith(
-	(f, res) =>
-		R.isNil(res) || R.isEmpty(res) ? res : Promise.resolve(res).then(f),
-)
-
-// the longitude and latitute of PregnancyCenter are in pc.address.location, which is a GeoJSON point
-// GeoJSON Point: { type: "Point", coordinates: [long, lat] }
-//
-
-// Google's geocoding API has the location part of the result in the format:
-// "location" : {
-//     "lat" : 37.4224764,
-//     "lng" : -122.0842499
-// }
-
-// string => response data as promise
-const getGoogleGeocode = fullAddress =>
-	googleMapsClient.geocode({ address: fullAddress }).asPromise() // a promise
-
-// response => location json or null
-const getLocation = response => {
-	try {
-		return response.json.results[0].geometry.location
-	} catch (err) {
-		log.info('response had no location')
-		return null
-	}
-}
-
-const saveLocation = async (pregnancyCenter, location) => {
-	log.info(`saving ${pregnancyCenter.prcName}`)
-	pregnancyCenter.address.location = {
-		type: 'Point',
-		coordinates: [location.lng, location.lat],
-	}
-	return pregnancyCenter.save() // a promise
-}
-
-const getFullAddress = pregnancyCenter => {
-	const address = pregnancyCenter.getFullAddress()
-	if (R.isEmpty(address)) log.info(`${pregnancyCenter.prcName} has no address`)
-	return address
-}
-
-=======
->>>>>>> rewrite helpers
-const geocodeAndSave = async pregnancyCenter => {
-	try {
-		return safePipeP([
-			getFullAddress,
-			getGoogleGeocode,
-			getLocation,
-<<<<<<< HEAD
-			R.partial(saveLocation, [pregnancyCenter]),
->>>>>>> updated Ramda and rewrote geocode with a version of pipeWith that checks for isNil or Empty and accepts promises
-=======
-			R.partial(addLocation, [pregnancyCenter]),
-			saveDoc,
->>>>>>> rewrite helpers
 		])(pregnancyCenter)
 	} catch (err) {
 		log.error(err)
