a312b2c2d5a5d9c86eb3be1235e93fc75982c8ce
Karen Rose <karen.rose807@gmail.com>
2019-04-10 08:52:10 -0700

    Add title

--- client/js/hah-app/about.js
+++ client/js/hah-app/about.js
@@ -1,7 +1,60 @@
 import React from 'react'
+import injectSheet from 'react-jss'
 
-const About = () => {
-	return <div>ABOUT</div>
+import Spacer from '../components/spacer'
+import Footer from '../mini-app/components/footer'
+import LogoAndNavigation from '../mini-app/logo-and-navigation'
+import { Phone, Desktop } from '../components/breakpoints'
+
+const TITLE_TEXT = 'What is Help Assist Her?'
+
+const BODY_TEXT =
+	'Some women may need help navigating the myriad of healthcare resources out there. ' +
+	"(Honestly, why isn't this easier?) Others may not know what facilities are available in their area. " +
+	'Help Assist Her is designed to help you or someone you know locate a holistic healthcare provider to address ' +
+	'your personal needs. Help Assist Her is here to connect you with them.'
+
+const SECOND_PARAGRAPH =
+	'Help Assist Her is a collection of up-to-date, local healthcare resources and ' +
+	'their contact details that will help you get in touch with the care you need.'
+
+const About = ({ classes }) => (
+	<div>
+		<LogoAndNavigation />
+
+		<Phone>
+			<div>
+				<Spacer height="36px" />
+				<div className={classes.titlePhone}>{TITLE_TEXT}</div>
+			</div>
+		</Phone>
+
+		<Desktop>
+			<div>
+				<Spacer height="60px" />
+				<div className={classes.titleDesktop}>{TITLE_TEXT}</div>
+			</div>
+		</Desktop>
+
+		<Footer />
+	</div>
+)
+
+const styles = {
+	titlePhone: {
+		color: '#000000',
+		'font-family': 'hah-regular',
+		'font-size': '24px',
+		'line-height': '28px',
+		'text-align': 'center',
+	},
+	titleDesktop: {
+		color: '#000000',
+		'font-family': 'hah-regular',
+		'font-size': '40px',
+		'line-height': '48px',
+		'text-align': 'center',
+	},
 }
 
-export default About
+export default injectSheet(styles)(About)
