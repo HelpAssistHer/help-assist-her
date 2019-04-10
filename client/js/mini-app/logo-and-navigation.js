6e383e603d78efd9f3e72bb4c01f04f6a7a3a83a
Karen Rose <karen.rose807@gmail.com>
2019-04-10 08:32:39 -0700

    Add bottom border to header

--- client/js/mini-app/logo-and-navigation.js
+++ client/js/mini-app/logo-and-navigation.js
@@ -10,25 +10,29 @@ import HamburgerMenu from '../components/icons/icon-components/hamburger-menu'
 const LogoAndNavigation = ({ classes }) => (
 	<div>
 		<Phone>
-			<div className={classes.logoPhone}>
-				<LogoBetaBlack height={25} width={160} />
-				<Link to="/menu" className={classes.hamburgerMenuPhone}>
-					<HamburgerMenu />
-				</Link>
+			<div className={classes.headerPhone}>
+				<div className={classes.logoPhone}>
+					<LogoBetaBlack height={25} width={160} />
+					<Link to="/menu" className={classes.hamburgerMenuPhone}>
+						<HamburgerMenu />
+					</Link>
+				</div>
 			</div>
 		</Phone>
 
 		<Desktop>
-			<div className={classes.logoDesktop}>
-				<LogoBetaBlack height={33} width={208} />
-				<div className={classes.linksDesktop}>
-					<Link to="/about" className={classes.navigationLinkDesktop}>
-						About
-					</Link>
-					<Spacer width="32px" />
-					<Link to="/feedback" className={classes.navigationLinkDesktop}>
-						Feedback
-					</Link>
+			<div className={classes.headerDesktop}>
+				<div className={classes.logoDesktop}>
+					<LogoBetaBlack height={33} width={208} />
+					<div className={classes.linksDesktop}>
+						<Link to="/about" className={classes.navigationLinkDesktop}>
+							About
+						</Link>
+						<Spacer width="32px" />
+						<Link to="/feedback" className={classes.navigationLinkDesktop}>
+							Feedback
+						</Link>
+					</div>
 				</div>
 			</div>
 		</Desktop>
@@ -36,6 +40,12 @@ const LogoAndNavigation = ({ classes }) => (
 )
 
 const styles = {
+	headerPhone: {
+		'border-bottom': '1px solid #3D65F9',
+	},
+	headerDesktop: {
+		'border-bottom': '4px solid #3D65F9',
+	},
 	logoPhone: {
 		height: '55px',
 		margin: '0px 16px 0px 17px',
