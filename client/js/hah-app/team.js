20fda89dbc774fdb930f6c2387b8599c76a2778c
Karen Rose <karen.rose807@gmail.com>
2019-04-10 13:13:37 -0700

    Add name and title text

--- client/js/hah-app/team.js
+++ client/js/hah-app/team.js
@@ -3,8 +3,38 @@ import injectSheet from 'react-jss'
 
 import { Phone, Desktop } from '../components/breakpoints'
 
-const Team = ({ classes }) => <div>THE TEAM</div>
+const NAME_AND_TITLE_TEXT = 'Karen Rose | Executive Director'
 
-const styles = {}
+const Team = ({ classes }) => (
+	<div className={classes.team}>
+		<Phone>
+			<div className={classes.nameAndTitleTextPhone}>{NAME_AND_TITLE_TEXT}</div>
+		</Phone>
+
+		<Desktop>
+			<div className={classes.nameAndTitleTextDesktop}>
+				{NAME_AND_TITLE_TEXT}
+			</div>
+		</Desktop>
+	</div>
+)
+
+const styles = {
+	team: {
+		'background-color': 'yellow',
+	},
+	nameAndTitleTextPhone: {
+		color: '#000000',
+		'font-family': 'hah-regular',
+		'font-size': '16px',
+		'line-height': '22px',
+	},
+	nameAndTitleTextDesktop: {
+		color: '#000000',
+		'font-family': 'hah-bold',
+		'font-size': '18px',
+		'line-height': '26px',
+	},
+}
 
 export default injectSheet(styles)(Team)
