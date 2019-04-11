a429f2772460721d59c5c2725d511dc92ae2aa73
Karen Rose <karen.rose807@gmail.com>
2019-04-10 23:14:07 -0700

    Add header and footer

--- client/js/hah-app/feedback.js
+++ client/js/hah-app/feedback.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { ReactTypeformEmbed } from 'react-typeform-embed'
+import LogoAndNavigation from '../mini-app/logo-and-navigation'
+import Footer from '../mini-app/components/footer'
 
 const Feedback = () => (
 	<div>
-		<ReactTypeformEmbed url="https://helpassisther.typeform.com/to/pUhArd" />
+		<LogoAndNavigation />
+		<ReactTypeformEmbed
+			url="https://helpassisther.typeform.com/to/pUhArd"
+			style={{ position: 'unset', height: '500px' }}
+		/>
+		<Footer />
 	</div>
 )
 
