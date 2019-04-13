3695f2ca7ac2ddba454caad0e5cf34e98b400723
Karen Rose <karen.rose807@gmail.com>
2019-04-13 12:02:18 -0700

    Add call 911 section

--- client/js/hah-app/emergency.js
+++ client/js/hah-app/emergency.js
@@ -11,31 +11,61 @@ const TITLE_TEXT = 'You are not alone. You are loved.'
 const BODY_TEXT =
 	'We have your back and so do the people working at the organizations below.'
 
+const CALL_EMERGENCY_TEXT = 'If this is an emergency, call'
+
+const EMERGENCY_NUMBER = '9-1-1'
+
+const PERIOD = '.'
+
 const Emergency = ({ classes }) => (
 	<div>
 		<LogoAndNavigation />
 
 		<Phone>
 			<div>
-				<Spacer height="33px" />
-				<div>Phone +</div>
-				<Spacer height="22px" />
-				<div className={classes.titlePhone}>{TITLE_TEXT}</div>
-				<Spacer height="22px" />
-				<div className={classes.bodyPhone}>{BODY_TEXT}</div>
-				<Spacer height="44px" />
+				<div className={classes.infoSectionPhone}>
+					<Spacer height="33px" />
+					<div>Phone +</div>
+					<Spacer height="22px" />
+					<div className={classes.titlePhone}>{TITLE_TEXT}</div>
+					<Spacer height="22px" />
+					<div className={classes.bodyPhone}>{BODY_TEXT}</div>
+					<Spacer height="44px" />
+				</div>
+				<div className={classes.call911SectionPhone}>
+					<Spacer height="25px" />
+					<div className={classes.textContainer}>
+						{CALL_EMERGENCY_TEXT}&nbsp;
+						<div className={classes.emergencyNumber}>{EMERGENCY_NUMBER}</div>
+						{PERIOD}
+					</div>
+					<Spacer height="25px" />
+				</div>
+				<Spacer height="45px" />sss
 			</div>
 		</Phone>
 
 		<Desktop>
 			<div>
-				<Spacer height="60px" />
-				<div>Phone +</div>
-				<Spacer height="44px" />
-				<div className={classes.titleDesktop}>{TITLE_TEXT}</div>
-				<Spacer height="44px" />
-				<div className={classes.bodyDesktop}>{BODY_TEXT}</div>
-				<Spacer height="77px" />
+				<div className={classes.infoSectionDesktop}>
+					<Spacer height="60px" />
+					<div>Phone +</div>
+					<Spacer height="44px" />
+					<div className={classes.titleDesktop}>{TITLE_TEXT}</div>
+					<Spacer height="44px" />
+					<div className={classes.bodyDesktop}>{BODY_TEXT}</div>
+					<Spacer height="77px" />
+				</div>
+				<div className={classes.call911SectionDesktop}>
+					<Spacer height="45px" />
+					<div className={classes.textContainer}>
+						{CALL_EMERGENCY_TEXT}&nbsp;
+						<div className={classes.emergencyNumber}>{EMERGENCY_NUMBER}</div>
+						{PERIOD}
+					</div>
+					<Spacer height="45px" />
+				</div>
+				<Spacer height="45px" />sss
 			</div>
 		</Desktop>
 
@@ -44,6 +74,14 @@ const Emergency = ({ classes }) => (
 )
 
 const styles = {
+	infoSectionPhone: {
+		'background-color': '#DEA8E0',
+		'border-bottom': '1px solid #3D65F9',
+	},
+	infoSectionDesktop: {
+		'background-color': '#DEA8E0',
+		'border-bottom': '3px solid #3D65F9',
+	},
 	titlePhone: {
 		color: '#000000',
 		'font-family': 'hah-regular',
@@ -71,6 +109,29 @@ const styles = {
 		'font-size': '24px',
 		'line-height': '35px',
 		'text-align': 'center',
+	},
+	call911SectionPhone: {
+		color: '#000000',
+		'font-family': 'hah-regular',
+		'font-size': '24px',
+		'line-height': '32px',
+		'text-align': 'center',
+		'border-bottom': '1px solid #3D65F9',
+	},
+	call911SectionDesktop: {
+		color: '#000000',
+		'font-family': 'hah-regular',
+		'font-size': '30px',
+		'line-height': '36px',
+		'text-align': 'center',
+		'border-bottom': '3px solid #3D65F9',
+	},
+	textContainer: {
+		display: 'flex',
+		'justify-content': 'center',
+	},
+	emergencyNumber: {
+		color: '#3D65F9',
 	},
 }
 
