f523f2441a3ff94ad7a7dbf9c02e2436f85ea561
Karen Rose <karen.rose807@gmail.com>
2019-04-13 20:07:48 -0700

    Add text

--- client/js/mini-app/home-page-banner.js
+++ client/js/mini-app/home-page-banner.js
@@ -2,14 +2,29 @@ import React from 'react'
 import injectSheet from 'react-jss'
 
 import { Desktop } from '../components/breakpoints'
+import Spacer from '../components/spacer'
+
+const OTHER_SERVICES_DESCRIPTION_TEXT =
+	'We’ve got your back. Among thousands of internet ' +
+	'search results, pamphlets, handouts, even old school phone books, we’ve found trustworthy ' +
+	'non-violent and holistic healthcare providers.'
 
 const HomePageBanner = ({ classes }) => (
 	<div>
 		<Desktop>
-			<div className={classes.bannerDesktop}>
-				Always there.
-				<br />
-				Just for you.
+			<div>
+				<div className={classes.bannerDesktop}>
+					Always there.
+					<br />
+					Just for you.
+				</div>
+				<Spacer height="72px" />
+				<div className={classes.textContainer}>
+					<div className={classes.otherServicesDescription}>
+						{OTHER_SERVICES_DESCRIPTION_TEXT}
+					</div>
+				</div>
+				<Spacer height="56px" />
 			</div>
 		</Desktop>
 	</div>
@@ -30,6 +45,19 @@ const styles = {
 		'line-height': '94px',
 		'letter-spacing': '-0.1px',
 		'text-align': 'center',
+	},
+	textContainer: {
+		display: 'grid',
+		'grid-template-columns': '20% 60% 20%',
+	},
+	otherServicesDescription: {
+		color: '#000000',
+		'font-family': 'hah-regular',
+		'font-size': '24px',
+		'line-height': '35px',
+		'text-align': 'center',
+		'grid-column-start': 2,
+		'grid-column-end': 3,
 	},
 }
 
