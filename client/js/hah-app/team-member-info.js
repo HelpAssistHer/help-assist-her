820b8799bf440815d00d822bd4bebe658f8d08af
Karen Rose <karen@buildingconnected.com>
2019-08-14 17:10:01 -0500

    Add bios

--- client/js/hah-app/team-member-info.js
+++ client/js/hah-app/team-member-info.js
@@ -2,48 +2,79 @@ module.exports = [
 	{
 		id: 0,
 		name: 'Karen R.',
-		title: 'Executive Director',
+		title: 'Executive Director and Lead Software Engineer',
 		bio:
-			'Karen is an engineering manager at a construction tech company in San Francisco. ' +
+			'Karen is a software engineering manager at a construction tech company in San Francisco. ' +
 			'Her own personal struggle with endometriosis and PCOS has made her extremely passionate ' +
 			"about women's healthcare. She is fulfilling her dream by volunteering her skills to help women " +
 			"more easily access good healthcare options. When she isn't staring at a computer screen, " +
 			'she enjoys traveling, running, wine tasting, and camping.',
-		imageSource: '../img/team/karen.png',
+		imageSource: '../img/team/karen.jpg',
 	},
 	{
 		id: 1,
-		name: 'Marylouise M.',
-		title: 'Rock Star Designer',
-		bio: 'Some stuff here',
-		imageSource: '../img/team/karen.png',
+		name: 'Kate S.',
+		title: 'Backend Software Engineer',
+		bio:
+			'Kate is a software engineer at Agoric with an interest in economics and law. She has been ' +
+			'a columnist for the Cato Institute and was previously a board member of the Tezos Commons Foundation. ' +
+			'She graduated from the University of California at Berkeley with a degree in Computer Science.',
+		imageSource: '../img/team/kate.jpg',
 	},
 	{
 		id: 2,
-		name: 'Kate S.',
-		title: 'Rock Star Developer',
+		name: 'Marylouise M.',
+		title: 'Graphic Designer',
 		bio: 'Some stuff here',
-		imageSource: '../img/team/karen.png',
+		imageSource: '../img/team/marylouise.jpg',
 	},
 	{
 		id: 3,
-		name: 'Destiny H.',
-		title: 'Rock Star Activist',
-		bio: 'Some stuff here',
-		imageSource: '../img/team/karen.png',
+		name: 'Valery Z.',
+		title: 'Frontend Software Engineer',
+		bio:
+			'Valery is a full stack developer at trade.io with experience building web apps using Ruby on Rails and React. ' +
+			'He started volunteering for Help Assist Her in 2018 as a frontend engineer to try to use his skill set as a developer ' +
+			"to help others who are in need. When he's not coding, Valery likes to lift weights, play chess, and travel.",
+		imageSource: '../img/team/valery.jpg',
 	},
 	{
 		id: 4,
-		name: 'Cessilye S.',
-		title: 'Rock Star Doula',
-		bio: 'Some stuff here',
-		imageSource: '../img/team/karen.png',
+		name: 'Rachael K.',
+		title: 'Copywriter',
+		bio:
+			"Rachael's day job is technical writing but when she clocks out, she transforms into a dog-training, sock-making, " +
+			"game-playing woman. She enjoys working with words in all forms, whether it's instructions for software or a novel. " +
+			'Her desire to help women (and men) know that they are cared for drives her and led her to help out with Help Assist Her.',
+		imageSource: '../img/team/rachael.jpg',
 	},
 	{
 		id: 5,
-		name: 'Valery Z.',
-		title: 'Rock Star Developer',
+		name: 'Julia F.',
+		title: 'Graphic Designer',
 		bio: 'Some stuff here',
-		imageSource: '../img/team/karen.png',
+		imageSource: '../img/team/julia.jpg',
+	},
+	{
+		id: 6,
+		name: 'Bhavya B.',
+		title: 'Frontend Software Engineer',
+		bio:
+			"Bhavya is a rising web developer passionate about creating something meaningful using modern technology. She has a bachelor's degree " +
+			'in Electronics and Communication Technology. Other than Help Assist Her, she also has been involved in Jubilee Reach, Code Fellows, ' +
+			'and Bellevue Community College. When she isn’t working, you’ll find her playing with her two adorable sons, making some form of arts and crafts, ' +
+			'listening to music, testing new cuisine, or trying new restaurants."',
+		imageSource: '../img/team/bhavya.jpg',
+	},
+	{
+		id: 7,
+		name: 'Veronica L.',
+		title: 'Researcher',
+		bio:
+			'Veronica is a Biomedical Engineer with an expanded focus in public health allowing her to bring dynamic, practical solutions to global health ' +
+			'projects. She also has developed data processing, analytics, and visualization skills through her research at University of Pittsburgh, ' +
+			'Rutgers University, and George Washington University. She is currently a private contractor designing, ' +
+			'monitoring, and evaluating projects from start to finish in health research labs in collaboration with USAID.',
+		imageSource: '../img/team/veronica.jpg',
 	},
 ]
