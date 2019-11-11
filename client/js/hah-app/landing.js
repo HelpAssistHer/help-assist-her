f60a50da851bf2b47cb647ee548e6557b9a7d7bf
Karen Rose <karen@buildingconnected.com>
2019-11-11 10:01:57 -0800

    Remove VerificationPortal link and add design tweaks

--- client/js/hah-app/landing.js
+++ client/js/hah-app/landing.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import injectSheet from 'react-jss'
 
@@ -10,22 +9,16 @@ class Landing extends React.Component {
 		const { classes } = this.props
 		return (
 			<div className={classes.landing}>
-				<Link to="/verification/pregnancy-resource-center">
-					Verification Portal
-				</Link>
-				<ul className={classes.homeScreen}>
-					<li>
-						<img
-							src="./img/logo-white.png"
-							alt="Help Assist Her logo"
-							className={classes.logoWhite}
-						/>
-					</li>
-					<li className={classes.textOne}>Information Database</li>
-					<li className={classes.textTwo}>
-						Sign in to start helping our sister out!
-					</li>
-				</ul>
+				<img
+					src="./img/logo-white.png"
+					alt="Help Assist Her logo"
+					className={classes.logoWhite}
+				/>
+
+				<div className={classes.titleText}>Information Database</div>
+				<div className={classes.descriptionText}>
+					Sign in to start helping our sisters out!
+				</div>
 				<LoginButton />
 			</div>
 		)
@@ -40,31 +33,18 @@ const styles = {
 		'background-position': 'center',
 		'background-repeat': 'no-repeat',
 		'background-size': 'cover',
-	},
-	homeScreen: {
-		position: 'absolute',
-		top: '20%',
-		margin: '0%',
-		padding: '0%',
-		width: '100%',
-		'list-style': 'none',
-		'text-align': 'center',
 		color: '#FFFFFF',
-		'font-family': 'Century Gothic',
-		'font-weight': 'bold',
+		'text-align': 'center',
 	},
 	logoWhite: {
-		height: '67.07px',
-		width: '338.23px',
+		width: '340px',
 	},
-	textOne: {
+	titleText: {
 		'font-size': '72px',
-		'letter-spacing': '3px',
 		'line-height': '88px',
 	},
-	textTwo: {
+	descriptionText: {
 		'font-size': '24px',
-		'letter-spacing': '0.33px',
 		'line-height': '29px',
 		'margin-top': '45px',
 	},
