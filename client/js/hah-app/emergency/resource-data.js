00091304868952055241100435190aa9a0514bbe
Karen Rose <karen@buildingconnected.com>
2019-11-28 13:44:19 -0800

    Update emergency resources

--- client/js/hah-app/emergency/resource-data.js
+++ client/js/hah-app/emergency/resource-data.js
@@ -1,11 +1,5 @@
 module.exports = [
 	{
-		nameOfResource: '24-Hour National Pregnancy Help Hotline',
-		phoneNumber: '1-800-712-4357',
-		additionalInfo: 'or text “HELPLINE” to 313131',
-		website: 'https://optionline.org',
-	},
-	{
 		nameOfResource: 'National Sexual Assault Hotline / Live Chat',
 		phoneNumber: '1-888-656-4673',
 		additionalInfo: 'live chat also available online',
@@ -14,31 +8,32 @@ module.exports = [
 	{
 		nameOfResource: 'National Domestic Violence Hotline / Live Chat',
 		phoneNumber: '1-888-799-7233',
-		additionalInfo: 'live chat also available',
+		additionalInfo: 'live chat also available online',
 		website: 'https://thehotline.org',
 	},
 	{
 		nameOfResource: 'National Human Trafficking Hotline',
 		phoneNumber: '1-888-373-7888',
-		additionalInfo: 'Send a text to 233733',
+		additionalInfo: 'live chat available online or send a text to 233733',
 		website: 'https://traffickingresourcecenter.org',
 	},
 	{
 		nameOfResource: 'National Suicide Prevention Lifeline',
 		phoneNumber: '1-800-273-8255',
-		additionalInfo: 'Live chat also available online',
+		additionalInfo: 'live chat also available online',
 		website: 'https://suicidepreventionlifeline.org/chat',
 	},
 	{
-		nameOfResource: 'SAMHSA Treatment Referral Helpline',
-		phoneNumber: '1-877-726-4727',
-		additionalInfo: 'Monday - Friday, 8am-8pm. More information online',
-		website: 'https://mentalhealth.gov/get-help/immediate',
+		nameOfResource: '24-Hour National Pregnancy Help Hotline',
+		phoneNumber: '1-800-712-4357',
+		additionalInfo: "live chat available online or text 'HELPLINE' to 313131",
+		website: 'https://optionline.org',
 	},
 	{
-		nameOfResource: 'Substance Abuse and Mental Health',
+		nameOfResource:
+			'Substance Abuse and Mental Health (SAMHSA) National Helpline',
 		phoneNumber: '1-800-662-4357',
 		additionalInfo: '',
-		website: '',
+		website: 'https://www.samhsa.gov/find-help/national-helpline',
 	},
 ]
