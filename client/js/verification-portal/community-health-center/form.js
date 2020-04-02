e1de7d22e100f971a62a5d9c064b8dc559d59398
Karen Rose <karen@buildingconnected.com>
2020-04-02 16:18:45 -0700

    Format toggles

--- client/js/verification-portal/community-health-center/form.js
+++ client/js/verification-portal/community-health-center/form.js
@@ -103,152 +103,191 @@ const CommunityHealthCenterForm = ({ classes }) => {
 				isSubmitting,
 			}) => (
 				<form className={classes.form} onSubmit={handleSubmit}>
-					<div>
-						<div className={classes.leftColumn}>VERIFIED</div>
-						<div className={classes.rightColumn}>GENERAL INFORMATION</div>
-					</div>
-					<Spacer height="50px" />
-					<div>
-						<div>
-							<ToggleFormik name="verifiedData.chcName.verified" />
+					<div className={classes.formSection}>
+						<div className={classes.gridField}>
+							<div className={classes.firstBox}>
+								<Heading text="VERIFIED" size="small" />
+							</div>
+							<div className={classes.headingGrid}>
+								<Heading text="GENERAL INFORMATION" size="medium" />
+							</div>
 						</div>
-
-						<Field
-							name="chcName"
-							placeholder="Name of Community Health Center"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.chcName}
-							as={Input}
-						/>
-						{errors.chcName && touched.chcName && errors.chcName}
-
 						<Spacer height="55px" />
 
-						<Field
-							name="addressLine1"
-							placeholder="Address 1"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.addressLine1}
-							as={Input}
-						/>
-						{errors.addressLine1 && touched.addressLine1 && errors.addressLine1}
+						<div className={classes.gridField}>
+							<div className={classes.firstBox}>
+								<ToggleFormik name="verifiedData.chcName.verified" />
+							</div>
 
-						<Field
-							name="addressLine2"
-							placeholder="Address 2"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.addressLine2}
-							as={Input}
-						/>
-
-						<Field
-							name="city"
-							placeholder="City"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.city}
-							as={Input}
-						/>
-						{errors.city && touched.city && errors.city}
-
-						<Field
-							name="state"
-							placeholder="State"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.state}
-							as={Input}
-						/>
-						{errors.state && touched.state && errors.state}
-
-						<div>
-							<ToggleFormik name="verifiedData.address.verified" />
+							<div className={classes.secondAndThirdBox}>
+								<Field
+									name="chcName"
+									placeholder="Name of Community Health Center"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.chcName}
+									as={Input}
+								/>
+								{errors.chcName && touched.chcName && errors.chcName}
+							</div>
 						</div>
 
-						<Field
-							name="zipCode"
-							placeholder="Zip Code"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.zipCode}
-							as={Input}
-						/>
-						{errors.zipCode && touched.zipCode && errors.zipCode}
+						<Spacer height="64px" />
 
-						<Spacer height="55px" />
+						<div className={classes.gridField}>
+							<div className={classes.secondAndThirdBox}>
+								<Field
+									name="addressLine1"
+									placeholder="Address 1"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.addressLine1}
+									as={Input}
+								/>
+								{errors.addressLine1 &&
+									touched.addressLine1 &&
+									errors.addressLine1}
+							</div>
 
-						<div>
-							<ToggleFormik name="verifiedData.phone.verified" />
+							<div className={classes.secondAndThirdBox}>
+								<Field
+									name="addressLine2"
+									placeholder="Address 2"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.addressLine2}
+									as={Input}
+								/>
+							</div>
+
+							<div className={classes.secondBox}>
+								<Field
+									name="city"
+									placeholder="City"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.city}
+									as={Input}
+								/>
+								{errors.city && touched.city && errors.city}
+							</div>
+
+							<div className={classes.thirdBox}>
+								<Field
+									name="state"
+									placeholder="State"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.state}
+									as={Input}
+								/>
+								{errors.state && touched.state && errors.state}
+							</div>
 						</div>
 
-						<Field
-							name="phoneNumber"
-							placeholder="Phone Number"
-							type="text"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.phoneNumber}
-							as={Input}
-						/>
-						{errors.phoneNumber && touched.phoneNumber && errors.phoneNumber}
+						<div className={classes.gridField}>
+							<div className={classes.firstBox}>
+								<ToggleFormik name="verifiedData.address.verified" />
+							</div>
 
-						<div>
-							<ToggleFormik name="verifiedData.email.verified" />
+							<div className={classes.secondBox}>
+								<Field
+									name="zipCode"
+									placeholder="Zip Code"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.zipCode}
+									as={Input}
+								/>
+								{errors.zipCode && touched.zipCode && errors.zipCode}
+							</div>
+						</div>
+						<Spacer height="66px" />
+
+						<div className={classes.gridField}>
+							<div className={classes.firstBox}>
+								<ToggleFormik name="verifiedData.phone.verified" />
+							</div>
+
+							<div className={classes.secondBox}>
+								<Field
+									name="phoneNumber"
+									placeholder="Phone Number"
+									type="text"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.phoneNumber}
+									as={Input}
+								/>
+								{errors.phoneNumber &&
+									touched.phoneNumber &&
+									errors.phoneNumber}
+							</div>
 						</div>
 
-						<Field
-							name="email"
-							placeholder="Email"
-							type="email"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.email}
-							as={Input}
-						/>
-						{errors.email && touched.email && errors.email}
+						<div className={classes.gridField}>
+							<div className={classes.firstBox}>
+								<ToggleFormik name="verifiedData.email.verified" />
+							</div>
 
-						<div>
-							<ToggleFormik name="verifiedData.website.verified" />
+							<div className={classes.secondBox}>
+								<Field
+									name="email"
+									placeholder="Email"
+									type="email"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.email}
+									as={Input}
+								/>
+								{errors.email && touched.email && errors.email}
+							</div>
 						</div>
 
-						<Field
-							name="website"
-							placeholder="Website (format is https://www.google.com)"
-							type="url"
-							onChange={handleChange}
-							onBlur={handleBlur}
-							value={values.website}
-							as={Input}
-						/>
-						{errors.website && touched.website && errors.website}
+						<div className={classes.gridField}>
+							<div className={classes.firstBox}>
+								<ToggleFormik name="verifiedData.website.verified" />
+							</div>
+
+							<div className={classes.secondBox}>
+								<Field
+									name="website"
+									placeholder="Website (format is https://www.google.com)"
+									type="url"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.website}
+									as={Input}
+								/>
+								{errors.website && touched.website && errors.website}
+							</div>
+						</div>
 
 						<Spacer height="81px" />
-
-						<div>
-							<Heading text="NOTES" size="medium" />
-							<Spacer height="50px" />
-							<Field name="notes" component="textarea" rows="4" cols="50" />
-						</div>
-
-						<Spacer height="50px" />
 					</div>
-					<Button
-						type="submit"
-						buttonText="Save Progress"
-						activeState={false}
-						size="large"
-						disabled={isSubmitting}
-					/>
-					<Spacer height="100px" />
+
+					<div className={classes.formSection}>
+						<Heading text="NOTES" size="medium" />
+						<Spacer height="50px" />
+						<Field name="notes" component="textarea" rows="4" cols="50" />
+					</div>
+
+					<div className={classes.formSection}>
+						<Spacer height="50px" />
+						<Button
+							type="submit"
+							buttonText="Save Progress"
+							activeState={false}
+							size="large"
+							disabled={isSubmitting}
+						/>
+					</div>
 				</form>
 			)}
 		</Formik>
@@ -256,31 +295,56 @@ const CommunityHealthCenterForm = ({ classes }) => {
 }
 
 const styles = {
+	parent: {
+		display: 'flex',
+		'align-items': 'baseline',
+	},
 	form: {
-		width: '80%',
-		'margin-left': '10%',
-		'font-family': 'sans-serif',
 		position: 'relative',
+		display: 'flex',
+		'flex-direction': 'column',
+		'align-items': 'baseline',
+		'justify-content': 'space-evenly',
 	},
-	leftColumn: {
-		display: 'inline-block',
-		width: '20%',
-		color: '#F48271',
-		height: '17px',
-		'font-size': '14px',
-		'font-weight': 'bold',
-		'line-height': '17px',
-		'letter-spacing': '2.61px',
+	formSection: {
+		width: '100%',
 	},
-	rightColumn: {
-		display: 'inline-block',
-		width: '80%',
-		color: '#D8D8D8',
-		'font-size': '20px',
-		'font-weight': 'bold',
-		'line-height': '24px',
+	blockFormEditing: {
+		'background-color': 'rgba(242,130,116,0.3)',
+		width: '104%',
+		height: '100%',
+		'z-index': '1',
+		display: 'block',
+		position: 'absolute',
+		top: '1%',
+		left: '-2%',
+	},
+	gridField: {
+		display: 'grid',
+		'grid-template-columns': '20% 50% 30%',
+	},
+	firstBox: {
+		'grid-column-start': 1,
+		'grid-column-end': 2,
+		'justify-self': 'center',
+		'align-self': 'center',
+	},
+	secondBox: {
+		'grid-column-start': 2,
+		'grid-column-end': 3,
+	},
+	thirdBox: {
+		'grid-column-start': 3,
+		'grid-column-end': 4,
+	},
+	secondAndThirdBox: {
+		'grid-column-start': 2,
+		'grid-column-end': 4,
+	},
+	headingGrid: {
+		'grid-column-start': 2,
+		'grid-column-end': 3,
 		'justify-self': 'start',
-		'letter-spacing': '3.73px',
 	},
 }
 
