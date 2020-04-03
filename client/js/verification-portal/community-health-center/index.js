d3767fef2ef55d2d6f7d490639baba3f3a482459
Karen Rose <karen@buildingconnected.com>
2020-04-02 18:28:18 -0700

    Add modal to CHC page

--- client/js/verification-portal/community-health-center/index.js
+++ client/js/verification-portal/community-health-center/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import CommunityHealthCenterForm from './form'
+import SuccessModal from '../components/success-modal'
 
 const CommunityHealthCenterContainer = () => (
 	<div>
+		<SuccessModal />
 		<CommunityHealthCenterForm />
 	</div>
 )
